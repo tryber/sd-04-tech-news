d16224a5efaddd41cc9ccf9a72b5b83f9da0c724
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-01-20 18:27:14 -0300

    req01

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,20 @@
+import requests
+# import time
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        # time.sleep(delay)
+        sleep(delay)
+        response.raise_for_status()
+    except requests.ReadTimeout:
+        return ""
+    except requests.HTTPError:
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):

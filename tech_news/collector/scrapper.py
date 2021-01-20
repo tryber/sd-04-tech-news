959200dce6b0de9fe02c8874b6963cd728e71fb9
igorgbr <igorgphotoarte@gmail.com>
2021-01-20 17:49:25 -0300

    req 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,17 +1,23 @@
 import requests
-import time
+from time import sleep
+from parsel import Selector
 
 
 def fetch_content(url, timeout=3, delay=0.5):
-
-    print(timeout)
-    for _ in range(15):
+    try:
         response = requests.get(url, timeout=timeout)
-        print(response)
-        time.sleep(delay)
+        sleep(delay)
+        response.raise_for_status()
+    except requests.ReadTimeout:
+        return ""
+    except requests.HTTPError:
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
 
-fetch_content("http://www.google.com")
+
+fetch_content("http://httpbin.org/status/404")

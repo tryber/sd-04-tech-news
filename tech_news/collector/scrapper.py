e144bf789375502d45dae1e723a24a36cd7a03f1
frederico-campello <fredfmc33@gmail.com>
2021-01-20 17:47:09 -0300

    Iniciando o projeto

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,21 @@
+import requests
+import time
+
+
+def sleep(secs):
+    time.sleep(secs)
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return ''
+    else:
+        return response.text
+    finally:
+        sleep(0.5)
 
 
 def scrape(fetcher, pages=1):

9c71289901fe04ca6edfdb97fcc790d0b8f99ea6
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2021-01-20 19:34:11 -0300

    req 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,15 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+        time.sleep(delay)
+        return response.text
+    except requests.ReadTimeout:
+        return ''
 
 
 def scrape(fetcher, pages=1):

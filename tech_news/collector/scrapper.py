98fd32941eeebc96f344e984e368a1d525580ceb
JorgeHSOsiro <jojimurakabe@gmail.com>
2021-01-20 19:12:55 -0300

    req 1?

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,23 @@
+import requests
+from time import sleep
+from parsel import Selector
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        sleep(delay)
+        response = requests.get(url, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        return ""
+    except requests.ReadTimeout:
+        return ""
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    URLBASE = "https://www.tecmundo.com.br/novidades"
+    next_page = '?page=2'
+    res = fetch_content(URLBASE + next_page)
+    # selector = Selector(text=res.text)
+    # url = selector.css("figure.tec--card__Thumb a::attr(href)").getall()
+    print(res)

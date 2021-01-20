da993a47ac555f199e60b3f114cc8c7323ac59fb
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-01-20 19:05:32 -0300

    completed requirement 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,13 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    response = requests.get(url, timeout=timeout)
+    if response.status_code != 200:
+        return ""
+    time.sleep(delay)
+    return response.text
 
 
 def scrape(fetcher, pages=1):

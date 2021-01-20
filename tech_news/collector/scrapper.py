0dddc926e4306fdd1fd59dd9e8bd5679a2489c22
Victor L <victor.lustosacp@gmail.com>
2021-01-20 15:44:41 -0300

    inciando

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,15 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    time.sleep(delay)
+    response = requests.get(url)
+    if response.status_code != 200:
+        return print('')
+    else:
+        print(response.status_code)  # código de status
+        print(response.text)  # conteúdo recebido
 
 
 def scrape(fetcher, pages=1):

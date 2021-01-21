66b3e69c536845fa0f6648c6d01d2f2b4d056324
henrique <hbaetaleite@gmail.com>
2021-01-21 19:29:20 -0300

    tentando passar no req 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,16 +1,18 @@
 import requests
 from time import sleep
+from parsel import Selector
 
 
 def fetch_content(url, timeout=3, delay=0.5):
     try:
         response = requests.get(url, timeout=timeout)
-        sleep(delay)
         response.raise_for_status()
     except (requests.HTTPError, requests.ReadTimeout):
         return ""
     else:
+        sleep(delay)
         return response.text
 
 
-# def scrape(fetcher, pages=1):
+def scrape(fetcher, pages=1):
+    

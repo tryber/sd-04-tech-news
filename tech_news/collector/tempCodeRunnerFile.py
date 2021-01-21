9e1ed0d7005c905c6cccd9c6f24c5dcf6d2d2118
Joao Soutto <joaosoutto@hotmail.com>
2021-01-21 16:47:37 -0300

    req 2

--- tech_news/collector/tempCodeRunnerFile.py
+++ tech_news/collector/tempCodeRunnerFile.py
@@ -1,5 +1,18 @@
 import requests
+from parsel import Selector
 
-response = requests.get("https://www.betrybe.com/")
-print(response.status_code)  # código de status
-print(response.headers["Content-Type"])
+BASE_URL = "https://www.tecmundo.com.br/novidades/"
+
+response = requests.get(BASE_URL + "?page=1")
+selector = Selector(text=response.text)
+
+href = selector.css(".tec--list__item h3 a::attr(href)").get()
+detail_page_url = BASE_URL + href
+
+detail_response = requests.get(detail_page_url)
+detail_selector = Selector(text=detail_response.text)
+
+description = detail_selector.css(".tec--article__header__title::text").get()
+print(description)
+
+# print(BASE_URL + href)

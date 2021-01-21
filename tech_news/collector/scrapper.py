ecf672bd9f32354345cf888c1b4abe39b0e9294d
igorgbr <igorgphotoarte@gmail.com>
2021-01-21 01:17:43 -0300

    dicionario pronto

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+from parsel import Selector
 
 
 def fetch_content(url, timeout=3, delay=0.5):
@@ -16,7 +17,53 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    summary = ""
+    sources = []
+    try:
+        url = "https://www.tecmundo.com.br/produto/209717-xiaomi-lanca-robo-varre-passa-pano-r-3-679-99-brasil.htm"
+        response = requests.get(url)
+        selector = Selector(text=response.text)
+
+        title = selector.css(".tec--article__header__title::text").get()
+        timestamp = selector.css(
+            ".tec--timestamp__item time::attr(datetime)"
+        ).get()
+        writer = selector.css(".tec--author__info__link::text").get()
+        shares_count = (
+            selector.css(".tec--toolbar__item::text")
+            .get()
+            .replace(" Compartilharam", "")
+        )
+        comments_count = selector.css(
+            ".tec--toolbar__item button::attr(data-count)"
+        ).get()
+
+        summary_list = selector.css(".tec--article__body p *::text").getall()
+        for i in summary_list:
+            summary += i
+
+        categories = selector.css("#js-categories .tec--badge::text").getall()
+
+        sources_list = selector.css(".z--mb-16 div *::text").getall()
+        for i in sources_list:
+            if i != " ":
+                sources.append(i)
+        noticie = {
+            "url": url,
+            "title": title,
+            "timestamp": timestamp,
+            "writer": writer,
+            "shares_count": int(shares_count),
+            "comments_count": int(comments_count),
+            "summary": summary,
+            "sources": sources,
+            "categories": categories,
+        }
+
+        print(noticie)
+
+    finally:
+        return ""
 
 
-fetch_content("http://httpbin.org/status/404")
+scrape("a")
---
+++ tech_news/collector/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+        noticie = {
+            "url": url,
+            "title": title,
+            "timestamp": timestamp,
+            "writer": writer,
+            "shares_count": int(shares_count),
+        }

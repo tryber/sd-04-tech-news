ce214bd20ec5a0e3fce9ebe8e5734290a94722fe
henrique <hbaetaleite@gmail.com>
2021-01-21 17:36:03 -0300

    first commit...

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,16 @@
+import requests
+from parsel import Selector
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    response = requests.get(url)
+    sleep(delay)
+    if response.status_code != 200:
+        return ""
+    else:
+        selector = Selector(text=response.text)
+        print(selector)
 
 
-def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+# def scrape(fetcher, pages=1):

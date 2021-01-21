6910fc3974e0212a25937439ff87b87cabf3ab26
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-01-21 18:05:35 -0300

    starting requirement 2

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+from parsel import Selector
 
 
 def fetch_content(url, timeout=3, delay=0.5):
@@ -10,8 +11,20 @@ def fetch_content(url, timeout=3, delay=0.5):
     except (requests.ReadTimeout, requests.HTTPError):
         return ""
     else:
+        sleep(delay)
         return response.text
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    page_url = fetcher
+    for _ in range(pages):
+        selector = Selector(text=page_url)
+        for news in selector.css(".tec--card__title"):
+            url = news.css("a::attr(href)").get()
+            details_selector = Selector(text=fetch_content(url))
+            details_selector.css("h1.tec--article__header__title::text").get(),
+        next_page_url = selector.css(".tec--btn::attr(href)").get()
+        page_url = fetch_content(str(next_page_url))
+
+
+scrape(fetch_content("https://www.tecmundo.com.br/novidades"))

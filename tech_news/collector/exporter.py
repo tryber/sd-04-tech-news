0db6f64b217aa43d9c2d18ca7f86f2f5f79bb783
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2021-01-21 20:29:36 -0300

    req 2 - 4

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,24 @@
+from ..database import find_news
+import csv
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    with open(filepath, "w") as file:
+        news = find_news()
+        header = [
+            "url",
+            "title",
+            "timestamp",
+            "writer",
+            "shares_count",
+            "comments_count",
+            "summary",
+            "sources",
+            "categories",
+        ]
+        writer = csv.writer(file)
+        writer.writerow(header)
+        for row in [[n[h] for n in news] for h in header]:
+            writer.writerow(row)
--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,3 +1,4 @@
+from parsel import Selector
 import requests
 import time
 
@@ -8,9 +9,66 @@ def fetch_content(url, timeout=3, delay=0.5):
         response.raise_for_status()
         time.sleep(delay)
         return response.text
-    except requests.ReadTimeout:
-        return ''
+    except (requests.ReadTimeout, requests.exceptions.HTTPError):
+        return ""
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    base_URL = "https://www.tecmundo.com.br/novidades?page="
+    scrape_list = []
+
+    for p in range(1, pages + 1):
+        selector = Selector(text=fetcher(base_URL + str(p)))
+        url_list = selector.css(
+            "h3.tec--card__title > a.tec--card__title__link::attr(href)"
+        ).getall()
+        scrape_list.extend({"url": url} for url in url_list)
+
+    def extractNumber(string):
+        return [int(i) for i in string.split() if i.isdigit()][0]
+
+    for scrape in scrape_list:
+        fetched = fetcher(scrape["url"])
+        selector = Selector(text=fetched)
+
+        scrape["title"] = selector.css("#js-article-title::text").get()
+
+        scrape["timestamp"] = selector.css(
+            "#js-article-date::attr(datetime)"
+        ).get()
+
+        scrape["writer"] = selector.css(
+            "a.tec--author__info__link::text"
+        ).get()
+        if scrape["writer"] is None:
+            print(fetched)
+        # scrape["shares_count"] = extractNumber(
+        #     selector.css("div.tec--toolbar__item::text").get()
+        # )
+
+        # scrape["comments_count"] = extractNumber(
+        #     selector.css(
+        #         "div.tec--toolbar__item > button::attr(data-count)"
+        #     ).get()
+        # )
+
+        # scrape["summary"] = selector.css(
+        #     "div.tec--article__body > p::text"
+        # ).get()
+
+        # scrape["sources"] = selector.css(
+        #     "a[class='tec--badge']::text"
+        # ).getall()
+
+        # scrape["categories"] = selector.css(
+        #     "a.tec--badge--primary::text"
+        # ).getall()
+
+    return scrape_list
+
+
+# scrape(fetch_content)
+# r = fetch_content(
+#   'https://www.tecmundo.com.br/minha-serie/209754-riverdale-saiba-tudo-estreia-5-temporada-recap.htm'
+# )
+# print(r)

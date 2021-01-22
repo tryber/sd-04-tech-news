7d68bedb4af04613b9ec359d9426cc016cf339a1
henrique <hbaetaleite@gmail.com>
2021-01-22 03:04:08 -0300

    testando menu de novo

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -2,17 +2,29 @@ def collector_menu():
     """Seu código deve vir aqui"""
     print("Selecione uma das opções a seguir:")
     print(" 1 - Importar notícias a partir de um arquivo CSV;")
-    print(
-        " 2 - Exportar notícias para CSV;\n 3 - Raspar notícias online;\n 4 - Sair."
-    )
+    print(" 2 - Exportar notícias para CSV;\n 3 - Raspar notícias online;")
+    print(" 4 - Sair.")
+
     while True:
         try:
             user_choice = int(input("Digite sua opção: "))
-        except (TypeError, ValueError, 1 <= user_choice >= 4):
+        except (TypeError, ValueError):
             print("Opção inválida")
             continue
         else:
+            # return user_choice
             print(user_choice)
+            if user_choice == 1:
+                print("Digite o nome do arquivo CSV a ser importado:")
+            elif user_choice == 2:
+                print("Digite o nome do arquivo CSV a ser exportado:")
+            elif user_choice == 3:
+                print("Digite a quantidade de páginas a serem raspadas:")
+            elif user_choice == 4:
+                print("Encerrando script")
+                break
+            else:
+                print("Opção inválida")
 
 
 def analyzer_menu():

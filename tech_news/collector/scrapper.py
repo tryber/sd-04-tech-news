d287bca151dcbed57bf15123ac54afa901afb61f
igorgbr <igorgphotoarte@gmail.com>
2021-01-21 23:03:36 -0300

    complexo

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -20,63 +20,61 @@ def scrape(fetcher, pages=1):
     summary = ""
     sources = []
 
-    response_principal = requests.get("https://www.tecmundo.com.br/novidades")
+    # response_principal = requests.get("https://www.tecmundo.com.br/novidades")
+    # selector_principal = Selector(text=response_principal.text)
 
-    selector_principal = Selector(text=response_principal.text)
+    # list_noticies_URL = selector_principal.css(
+    #     ".tec--card__title a::attr(href)"
+    # ).getall()
 
-    list_noticies_URL = selector_principal.css(
-        ".tec--card__title a::attr(href)"
-    ).getall()
+    # for i in list_noticies_URL:
+    url = "https://www.tecmundo.com.br/minha-serie/209762-bridgerton-netflix-luta-retirar-cenas-sexo-serie-sites-porno.htm"
+    response = requests.get(url)
+    selector = Selector(text=response.text)
 
-    for i in list_noticies_URL:
-        url = i
-        response = requests.get(url)
-        selector = Selector(text=response.text)
-
-        title = selector.css(".tec--article__header__title::text").get()
-        timestamp = selector.css(
-            ".tec--timestamp__item time::attr(datetime)"
-        ).get()
-        writer = selector.css(".tec--author__info__link::text").get()
-        if writer is None:
-            writer = selector.css(
-                ".z--items-center .tec--timestamp  .z--font-bold a::text"
-            ).get()
-
-        shares_count = (
-            selector.css(".tec--toolbar__item::text")
-            .get()
-            .replace(" Compartilharam", "")
-        )
-        if shares_count is None:
-            shares_count = 0
-
-        comments_count = selector.css(
-            ".tec--toolbar__item button::attr(data-count)"
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+    writer = selector.css(".tec--author__info__link::text").get()
+    if writer is None:
+        writer = selector.css(
+            ".z--items-center .tec--timestamp  .z--font-bold a::text"
         ).get()
 
-        summary_list = selector.css(".tec--article__body *::text").getall()
-        for i in summary_list:
-            summary += i
+    shares_count = (
+        selector.css(".tec--toolbar__item::text")
+        .get()
+        .replace(" Compartilharam", "")
+    )
+    if shares_count is None:
+        shares_count = 0
 
-        categories = selector.css("#js-categories .tec--badge::text").getall()
+    comments_count = selector.css(
+        ".tec--toolbar__item button::attr(data-count)"
+    ).get()
 
-        sources_list = selector.css(".z--mb-16 div *::text").getall()
-        for i in sources_list:
-            if i != " ":
-                sources.append(i)
-        noticies = []
-        noticie = {
-            "url": url,
-            "title": title,
-            "timestamp": timestamp,
-            "writer": writer,
-            "shares_count": int(shares_count),
-            "comments_count": int(comments_count),
-            "summary": summary,
-            "sources": sources,
-            "categories": categories,
-        }
-        noticies.append(noticie)
-        # print(noticie)
-        return noticies
+    summary_list = selector.css(".tec--article__body *::text").getall()
+    for i in summary_list:
+        summary += i
+
+    categories = selector.css("#js-categories .tec--badge::text").getall()
+
+    sources_list = selector.css(".z--mb-16 div *::text").getall()
+    for i in sources_list:
+        if i != " ":
+            sources.append(i)
+    noticies = []
+    noticie = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+    noticies.append(noticie)
+    return noticies

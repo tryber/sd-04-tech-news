e78e364ec7a003062343c8ceefab71cb1fea9792
henrique <hbaetaleite@gmail.com>
2021-01-22 02:50:16 -0300

    arriscando um pouco no menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,6 +1,22 @@
 def collector_menu():
     """Seu código deve vir aqui"""
+    print("Selecione uma das opções a seguir:")
+    print(" 1 - Importar notícias a partir de um arquivo CSV;")
+    print(
+        " 2 - Exportar notícias para CSV;\n 3 - Raspar notícias online;\n 4 - Sair."
+    )
+    while True:
+        try:
+            user_choice = int(input("Digite sua opção: "))
+        except (TypeError, ValueError, 1 <= user_choice >= 4):
+            print("Opção inválida")
+            continue
+        else:
+            print(user_choice)
 
 
 def analyzer_menu():
     """Seu código deve vir aqui"""
+
+
+collector_menu()

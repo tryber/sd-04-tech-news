25af227d7f9f6bba6d2f70494658a6f028941aae
henrique <hbaetaleite@gmail.com>
2021-01-22 01:55:30 -0300

    teste com a url especifica da noticia

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,7 +1,7 @@
 import requests
 from time import sleep
 
-# from parsel import Selector
+import parsel
 
 
 def fetch_content(url, timeout=3, delay=0.5):
@@ -15,4 +15,30 @@ def fetch_content(url, timeout=3, delay=0.5):
         return response.text
 
 
-# def scrape(fetcher, pages=1):
+def scrape(fetcher, pages=1):
+    selector = parsel.Selector(fetcher)
+    news = []
+    for notice in selector.css("div.tec--list__item"):
+        url = notice.css("a.tec--card__title__link::attr(href)").get()
+        title = notice.css("h3.tec--card__title *::text").get()
+        pageCode = parsel.Selector(url)
+        timestamp = notice.css("time::attr(datetime)").get()
+        # for one_notice in pageCode:
+        news.append(
+            {
+                "url": url,
+                "title": title.strip(),
+                "timestamp": timestamp,
+            }
+        )
+
+    return len(news)
+
+
+teste = fetch_content("https://www.tecmundo.com.br/produto/209787-microsoft-registra-tela-ajusta-angulo-visao-automaticamente.htm")
+
+# print(teste)
+
+respo = scrape(teste)
+
+print(respo)

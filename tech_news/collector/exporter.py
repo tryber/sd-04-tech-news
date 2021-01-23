656fd738d44f7f074843c6db5040f67081ccf332
Joao Soutto <joaosoutto@hotmail.com>
2021-01-23 19:36:49 -0300

    req 1 ao 4 passando no local

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,40 @@
+import csv
+from tech_news.database import find_news
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+
+    with open(filepath, "w") as file:
+
+        headers = [
+            "url",
+            "title",
+            "timestamp",
+            "writer",
+            "shares_count",
+            "comments_count",
+            "summary",
+            "sources",
+            "categories",
+        ]
+
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow(headers)
+        news = find_news()
+
+        for newNews in news:
+            writer.writerow(
+                [
+                    newNews["url"],
+                    newNews["title"],
+                    newNews["timestamp"],
+                    newNews["writer"],
+                    newNews["shares_count"],
+                    newNews["comments_count"],
+                    newNews["summary"],
+                    ','.join(newNews["sources"]),
+                    ','.join(newNews["categories"]),
+                ]
+            )
--- tech_news/collector/tempCodeRunnerFile.py
+++ tech_news/collector/tempCodeRunnerFile.py
@@ -1,18 +1,1 @@
-import requests
-from parsel import Selector
-
-BASE_URL = "https://www.tecmundo.com.br/novidades/"
-
-response = requests.get(BASE_URL + "?page=1")
-selector = Selector(text=response.text)
-
-href = selector.css(".tec--list__item h3 a::attr(href)").get()
-detail_page_url = BASE_URL + href
-
-detail_response = requests.get(detail_page_url)
-detail_selector = Selector(text=detail_response.text)
-
-description = detail_selector.css(".tec--article__header__title::text").get()
-print(description)
-
-# print(BASE_URL + href)
+print("ola")

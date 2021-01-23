694eec6aeb5f5de2d002c9f3391c9b16e756913b
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-01-23 20:08:38 -0300

    completed requirement 2

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -16,15 +16,44 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
+    pages_details = []
     page_url = fetcher
     for _ in range(pages):
         selector = Selector(text=page_url)
         for news in selector.css(".tec--card__title"):
             url = news.css("a::attr(href)").get()
             details_selector = Selector(text=fetch_content(url))
-            details_selector.css("h1.tec--article__header__title::text").get(),
+            page_details = {
+                "url": url,
+                "title": details_selector.css(
+                    "h1.tec--article__header__title::text"
+                ).get(),
+                "timestamp": details_selector.css(
+                    "#js-article-date::attr(datetime)"
+                ).get(),
+                "writer": details_selector.css(
+                    ".tec--author__info__link::text"
+                ).get(),
+                "shares_count": str(
+                    details_selector.css(".tec--toolbar__item::text").get()
+                )[1],
+                "comments_count": details_selector.css(
+                    "#js-comments-btn::attr(data-count)"
+                ).get(),
+                "summary": details_selector.css(
+                    ".tec--article__body p *::text"
+                ).get(),
+                "sources": details_selector.css(
+                    ".z--mb-16 .tec--badge::text"
+                ).getall(),
+                "categories": details_selector.css(
+                    "#js-categories .tec--badge::text"
+                ).getall(),
+            }
+            pages_details.append(page_details)
         next_page_url = selector.css(".tec--btn::attr(href)").get()
         page_url = fetch_content(str(next_page_url))
+        print(pages_details)
 
 
 scrape(fetch_content("https://www.tecmundo.com.br/novidades"))

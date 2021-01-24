25d8e08edf3198db16695bcbacaf26f4f3680268
gabrielukke <gabriellukk@outlook.com>
2021-01-24 06:43:46 -0300

    Req 1 - Tests Passed

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,9 +1,22 @@
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        sleep(delay)
+        response.raise_for_status()
+    except requests.ReadTimeout:
+        return ""
+    except requests.HTTPError:
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
 
 
-# For Commit...
+print(fetch_content("https://www.tecmundo.com.br/404"))

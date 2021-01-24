71d605ac3f2b1b6b3c4c82cd4c7a68ff74462f93
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-01-24 20:24:17 -0300

    req 5 title partial 02

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,10 +1,11 @@
 # from pymongo import MongoClient
 from tech_news import database
+import datetime
 
 
 def search_by_title(title):
     output_list = []
-    news = database.search_news({"title": {"$regex": title}})
+    news = database.search_news({"title": {"$regex": title, "$options": "-i"}})
     if len(news) == 1:
         output_list.append((news[0]["title"], news[0]["url"]))
 
@@ -12,10 +13,15 @@ def search_by_title(title):
 
 
 def search_by_date(date):
-    output_list = []
-    news = database.search_news({"timestamp": {"$regex": date}})
-    if len(news) == 1:
-        output_list.append((news[0]["timestamp"], news[0]["url"]))
+    try:
+        newDate = datetime.datetime.strptime(date, "%Y-%m-%d")
+        print(newDate)
+        output_list = []
+        news = database.search_news({"timestamp": {"$regex": date}})
+        if len(news) == 1:
+            output_list.append((news[0]["title"], news[0]["url"]))
+    except ValueError:
+        raise ValueError("Data inválida")
 
     return output_list
 

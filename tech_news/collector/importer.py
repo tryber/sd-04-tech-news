ee76130360557ad27feda747d48b4f38f22eac84
Filipe de Castro <filipeyoga@gmail.com>
2021-01-24 16:31:45 -0300

    req-3-4

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -2,8 +2,31 @@ import csv
 
 
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
 
-    with open("balneabilidade.csv") as file:
-        news = csv.reader(file, delimiter=";")
-        header, *data = news
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+
+    try:
+        with open(filepath, encoding="utf-8") as file:
+            default = [
+                "url",
+                "title",
+                "timestamp",
+                "writer",
+                "shares_count",
+                "comments_count",
+                "summary",
+                "sources",
+                "categories",
+            ]
+            header, *data = csv.reader(file, delimiter=";")
+
+            if header != default:
+                raise ValueError("Formato invalido")
+
+            return [
+                {header[i]: info[i] for i in range(len(info))} for info in data
+            ]
+
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")

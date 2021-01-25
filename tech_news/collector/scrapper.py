84e14aeafa21d7532e6c9d9fb954242fbd1ccda5
Tereza Cristina Carvalho <terezacristina@MacBook-Pro-de-Tereza.local>
2021-01-24 21:17:18 -0300

    requisito 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,25 @@
+import requests
+import time
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    time.sleep(delay)
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()  # retorna 1 objeto HTTPError
+        sleep(delay)
+    except (requests.exceptions.HTTPError, requests.exceptions.ReadTimeout):
+        return ""
+    else:
+        return response.text
+    '''
+    response = requests.get(url, timeout=timeout)
+    if response.status_code != 200:
+        return ''
+    time.sleep(delay)
+    return response.text
+    '''
 
 
 def scrape(fetcher, pages=1):

d8f299308731c61ef9be8c3fd36e5cdd9261341e
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-01-25 18:58:02 -0300

    starting scrapping

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,18 @@
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    """Seu código deve vir aqu
+    i"""
+    response = requests.get(url, timeout=timeout)
+    sleep(delay)
+    print(response.text)
+    return response.text
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
+
+
+fetch_content('https://g1.globo.com')

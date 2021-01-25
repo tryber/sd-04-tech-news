f2cd551efd197e3e8c09b6a63c28e7f62171d61c
juniomelos <juniomelos@gmail.com>
2021-01-25 14:12:45 -0300

    req 01

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,20 @@
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        print(response)
+        sleep(delay)
+        print(response.status_code)
+    except requests.ReadTimeout:
+        return ""
+    if response.status_code != 200:
+        return ""
+    else:
+        return response.text
 
 
-# def scrape(fetcher, pages=1):
+def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""

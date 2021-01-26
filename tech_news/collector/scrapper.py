bb300477af282694815d77613afdeedcfa1aa0dc
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-01-25 21:35:37 -0300

    Requisito 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -6,7 +6,7 @@ def fetch_content(url, timeout=3, delay=0.5):
     try:
         """requisição do tipo GET"""
         response = requests.get(url, timeout=timeout)
-        """pedindo que a resposta lance uma exceção caso o status não seja ok"""
+        """pedindo que a resposta lance uma exceção se o status não seja ok"""
         response.raise_for_status()
         """Coloca uma pausa de 0.5 segundos a cada requisição"""
         time.sleep(delay)

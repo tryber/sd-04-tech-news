b7d648ae3ef57d5c6891c05b4e5259da45004ac0
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-01-26 19:50:30 -0300

    Requisito 3

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,5 @@
 import requests
-import time
+from time import sleep
 from parsel import Selector
 
 
@@ -7,10 +7,10 @@ def fetch_content(url, timeout=3, delay=0.5):
     try:
         """requisição do tipo GET"""
         response = requests.get(url, timeout=timeout)
+        """Coloca uma pausa de 0.5 segundos a cada requisição"""
+        sleep(delay)
         """pedindo que a resposta lance uma exceção se o status não seja ok"""
         response.raise_for_status()
-        """Coloca uma pausa de 0.5 segundos a cada requisição"""
-        time.sleep(delay)
     except (requests.HTTPError, requests.ReadTimeout):
         """Em caso de erro retorna uma string vazia"""
         return ""
@@ -20,19 +20,34 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    URL_BASE = "https://www.tecmundo.com.br/novidades?page="
+    URL_BASE = "https://www.tecmundo.com.br/novidades"
     tech_news_list = []
+
     for page in range(1, pages + 1):
-        main_selector = Selector(fetcher(f"{URL_BASE}{page}"))
+        main_selector = Selector(fetcher(f"{URL_BASE}?page={page}"))
         articles_url_list = main_selector.css(
             ".tec--list__item .tec--card__title__link::attr(href)"
         ).getall()
 
         for url in articles_url_list:
             snd_selector = Selector(fetcher(url))
-            writer = snd_selector.css(
-                ".z--items-center .tec--timestamp .z--font-bold a::text"
-            ).get()
+
+            writer = snd_selector.css(".tec--author__info__link::text").get()
+            if writer is None:
+                writer = snd_selector.css(
+                    ".z--items-center .tec--timestamp  .z--font-bold a::text"
+                ).get()
+
+            shares_count = snd_selector.css(".tec--toolbar__item::text").get()
+            if shares_count is None:
+                shares_count = 0
+            else:
+                shares_count = (
+                    snd_selector.css(".tec--toolbar__item::text")
+                    .get()
+                    .replace(" Compartilharam", "")
+                )
+
             tech_news_list.append(
                 {
                     "url": url,
@@ -40,12 +55,10 @@ def scrape(fetcher, pages=1):
                         ".tec--article__header__title::text"
                     ).get(),
                     "timestamp": snd_selector.css(
-                        ".tec--timestamp__item::attr(datetime)"
+                        ".tec--timestamp__item time::attr(datetime)"
                     ).get(),
                     "writer": writer,
-                    "shares_count": int(
-                        snd_selector.css(".tec--toolbar__item::text").get()
-                    ),
+                    "shares_count": int(shares_count),
                     "comments_count": int(
                         snd_selector.css(
                             ".tec--toolbar__item button::attr(data-count)"

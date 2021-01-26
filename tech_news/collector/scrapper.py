d95f0c6b3b479abb28faa4191dfe04265f0afc2b
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-01-25 21:29:44 -0300

    iniciando o projeto tech-news

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,21 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        """requisição do tipo GET"""
+        response = requests.get(url, timeout=timeout)
+        """pedindo que a resposta lance uma exceção caso o status não seja ok"""
+        response.raise_for_status()
+        """Coloca uma pausa de 0.5 segundos a cada requisição"""
+        time.sleep(delay)
+    except (requests.HTTPError, requests.ReadTimeout):
+        """Em caso de erro retorna uma string vazia"""
+        return ""
+    else:
+        """conteudo recebido da requisição"""
+        return response.text
 
 
 def scrape(fetcher, pages=1):

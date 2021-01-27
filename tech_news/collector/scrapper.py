064b1eb44f64a18b2b81c5aa8836fa74040d4d64
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-27 18:07:01 -0300

    Second requirement test

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,4 +1,5 @@
 import requests
+import parsel
 from time import sleep
 
 
@@ -12,5 +13,40 @@ def fetch_content(url, timeout=3, delay=0.5):
         return response.text
 
 
+def extract_news_page(selector, url):
+    title = selector.css("h1.tec--article__header__title::text").get()
+    timestamp = selector.css("time#js-article-date::attr(datetime)").get()
+    writer = selector.css("a.tec--author__info__link::text").get()
+    shares_count = selector.css(".tec--toolbar__item::text").re_first(r"\d+")
+    comments_count = selector.css("#js-comments-btn::text").re_first(r"\d+")
+    summary = selector.css("div.tec--article__body > p::text").get()
+    sources = selector.css("div.z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("#js-categories a::text").getall()
+    page = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+    return page
+
+
+URL_BASE = "https://www.tecmundo.com.br/"
+
+
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    news_page = []
+    selector = parsel.Selector(fetcher(URL_BASE + "novidades/"))
+    for _ in range(pages):
+        urls = selector.css(
+            ".tec--list__item .tec--card__title__link::attr(href)"
+        ).getall()
+        for url in urls:
+            new_selector = parsel.Selector(fetcher(url))
+            news_page.append(extract_news_page(new_selector, url))
+    return news_page

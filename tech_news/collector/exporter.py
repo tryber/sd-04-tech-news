3e50e7d4f7942123d9792ef1660a258220a92320
igorgbr <igorgphotoarte@gmail.com>
2021-01-27 11:12:52 -0300

    req 4

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,35 @@
+import csv
+from tech_news.database import find_news
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    data = find_news()
+    if filepath.endswith(".csv"):
+        with open(filepath, "w", newline="") as csvfile:
+            writer = csv.DictWriter(
+                csvfile,
+                delimiter=";",
+                quotechar='"',
+                fieldnames=[
+                    "url",
+                    "title",
+                    "timestamp",
+                    "writer",
+                    "shares_count",
+                    "comments_count",
+                    "summary",
+                    "sources",
+                    "categories",
+                ],
+            )
+
+            for field in data:
+                for key in field:
+                    if isinstance(field[key], list):
+                        field[key] = ",".join(field[key])
+
+            writer.writeheader()
+            writer.writerows(data)
+            csvfile.close()
+    else:
+        raise ValueError("Formato invalido")

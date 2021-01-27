8fe53566bfedbf153ec38493f6197a6affa0ab2f
deboracosilveira <deboracosilveira@gmail.com>
2021-01-26 21:08:33 -0300

    Req 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,17 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+    except requests.ReadTimeout:
+        return ""
+    if response.status_code == 200:
+        return response.text
+    else:
+        return ""
 
 
 def scrape(fetcher, pages=1):

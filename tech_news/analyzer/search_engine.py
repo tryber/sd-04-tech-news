fc32ca721248d4b1d147cdede1cf0e71c519af95
Tereza Cristina Carvalho <terezacristina@MacBook-Pro-de-Tereza.local>
2021-01-27 12:13:16 -0300

    adaptando /modificando os requisitos de 5 ao 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,36 +1,39 @@
-import datetime
-from tech_news.database import search_news, find_news
+from tech_news.database import search_news
+from datetime import datetime
+
+
+def format_tuples(news_data):
+    if news_data == []:
+        return []
+    search_for_news = []
+    for new in news_data:
+        search_for_news.append((new["title"], new["url"]))
+        return search_for_news
 
 
 def search_by_title(title):
-    tuples_list = []
-    data = search_news({"title": {"$regex": title, "$options": "i"}})
-    for new in data:
-        tuples_list.append((new["title"], new["url"]))
-    return tuples_list
+    news_data = search_news({"title": {"$regex": title, "$options": "i"}})
+    return format_tuples(news_data)
 
 
 def search_by_date(date):
     try:
-        datetime.datetime.strptime(date, "%Y-%m-%d")
+        if datetime.strptime(date, "%Y-%m-%d"):
+            news_data = search_news(
+                {"timestamp": {"$regex": date, "$options": "i"}}
+            )
+            return format_tuples(news_data)
     except ValueError:
         raise ValueError("Data inválida")
-    else:
-        tuples_list = []
-        data = search_news({"timestamp": {"$regex": date}})
-        for new in data:
-            tuples_list.append((new["title"], new["url"]))
-        return tuples_list
 
 
 def search_by_source(source):
-    tuples_list = []
-    print(find_news())
-    data = search_news({"sources": {"$regex": source, "$options": "i"}})
-    for new in data:
-        tuples_list.append((new["title"], new["url"]))
-    return tuples_list
+    news_data = search_news({"sources": {"$regex": source, "$options": "i"}})
+    return format_tuples(news_data)
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news_data = search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
+    )
+    return format_tuples(news_data)

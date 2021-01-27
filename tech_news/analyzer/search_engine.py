19536a454c0db031cfff1717968b572afd6ea55a
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-01-26 21:38:47 -0300

    Requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,25 @@
+from tech_news.database import search_news
+from datetime import datetime
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    found_titles = search_news({"title": {"$regex": title, "$options": "i"}})
+    if found_titles == []: return []
+    search_title = []
+    for item in found_titles:
+        search_title.append((item["title"], item["url"]))
+    return search_title
 
-
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(date):   
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        found_news = search_news({"timestamp": {"$regex": date}})
+        if found_news == []: return []
+        news = []
+        for item in found_news:
+            news.append((item["title"], item["url"]))
+        return news
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 def search_by_source(source):

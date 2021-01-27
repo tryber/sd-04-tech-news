5fbe2e9275c3b219b7beaca00279523d4d776682
igorgbr <igorgphotoarte@gmail.com>
2021-01-27 20:47:26 -0300

    req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import db
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -6,13 +7,22 @@ def search_by_title(title):
 
     news_data = []
     for i in data:
-        news_data.append((i['title'], i['url']))
+        news_data.append((i["title"], i["url"]))
 
     return news_data
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        if datetime.strptime(date, "%Y-%m-%d"):
+            data = db.news.find({"timestamp": {"$regex": date}})
+            news_data = []
+            for i in data:
+                news_data.append((i["title"], i["url"]))
+
+        return news_data
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 def search_by_source(source):

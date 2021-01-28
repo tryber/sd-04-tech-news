587254e19f0874518b01704956db2584ae20ca6b
mirellaalves <mirella.alves@gmail.com>
2021-01-27 23:04:40 -0300

    requirement 2, try to get shares_count and comments_count as integer type

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+from parsel import Selector
 
 
 def fetch_content(url, timeout=3, delay=0.5):
@@ -14,4 +15,43 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    url_base = "https://www.tecmundo.com.br/novidades"
+    page = 1
+    next_page_url = f"?page={page}"
+    news = []
+
+    while page <= pages:
+        response = fetcher(url_base + next_page_url)
+        selector = Selector(text=response)
+
+        selector_attribute = ".tec--list__item h3 a::attr(href)"
+        for news_item in selector.css(selector_attribute).getall():
+            news_response = fetcher(news_item)
+            news_selector = Selector(text=news_response)
+
+            url = news_item
+            title = news_selector.css(".tec--article__header__title::text")
+            # timestamp = news_selector.css
+            # (".tec--timestamp__item time::attr(datetime)")
+            writer = news_selector.css(".tec--author__info__link::text")
+            shares_count = news_selector.css
+            (".tec--toolbar__item::attr(data-count)")
+            comments_count = news_selector.css(".tec--btn::attr(data-count)")
+            summary = news_selector.css(".tec--article__body p::text")
+            sources = news_selector.css(".z--mb-16 a::text")
+            categories = news_selector.css("#js-categories a::text")
+
+            news.append({
+                "url": url,
+                "title": title.get(),
+                "timestamp": news_selector.css
+                (".tec--timestamp__item time::attr(datetime)").get(),
+                "writer": writer.get(),
+                "shares_count": shares_count,
+                "comments_count": comments_count,
+                "summary": summary.get(),
+                "sources": sources.getall(),
+                "categories": categories.getall(),
+            })
+        page += 1
+    return news

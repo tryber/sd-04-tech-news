d1327c40b09e9c9adfa271260d5101a95a757f42
henrique <hbaetaleite@gmail.com>
2021-01-28 16:17:00 -0300

    agora eu entendi o importer!

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,7 @@
+import csv
+
+
 def csv_exporter(filepath):
     """Seu código deve vir aqui"""
+    with open(filepath, "w", newline="") as file:
+        writer = csv.writer(file)
--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -3,10 +3,30 @@ import csv
 
 def csv_importer(filepath):
     """Seu código deve vir aqui"""
-    with open(filepath) as csv_file:
-        reader = csv.reader(csv_file, delimiter=";")
-        try:
-            for line in reader:
-                print(line)
-        except csv.Error as err:
-            print("algum erro rolou...", err)
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato inválido")
+    try:
+
+        with open(filepath, encoding="utf-8") as csv_file:
+            needed_header = [
+                "url",
+                "title",
+                "timestamp",
+                "writer",
+                "shares_count",
+                "comments_count",
+                "summary",
+                "sources",
+                "categories",
+            ]
+            header, *data = csv.reader(csv_file, delimiter=";")
+
+            if header != needed_header:
+                raise ValueError("Formato inválido")
+
+            return [
+                {header[i]: info[i] for i in range(len(info))} for info in data
+            ]
+
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")

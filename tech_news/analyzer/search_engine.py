dd4ffbfceeca519e3489215a8e366ec1a59a4f74
igorgbr <igorgphotoarte@gmail.com>
2021-01-27 21:14:56 -0300

    req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,10 @@
 from tech_news.database import db
 from datetime import datetime
+import re
 
 
 def search_by_title(title):
-    data = db.news.find({"title": title.capitalize()})
+    data = db.news.find({"title": re.compile(title, re.IGNORECASE)})
 
     news_data = []
     for i in data:
@@ -26,7 +27,13 @@ def search_by_date(date):
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    data = db.news.find({"sources": re.compile(source, re.IGNORECASE)})
+
+    news_data = []
+    for i in data:
+        news_data.append((i["title"], i["url"]))
+
+    return news_data
 
 
 def search_by_category(category):

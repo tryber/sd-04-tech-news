e64b753399c22336be1c2fb6ed950dad724df1a8
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-01-27 23:59:37 -0300

    fix/lint

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -2,11 +2,14 @@ from tech_news import database
 
 
 def search_by_title(title):
-    db_result = database.search_news({"title": {"$regex": title, "$options": "i"}})
+    db_result = database.search_news({
+        "title": {"$regex": title, "$options": "i"}
+    })
     search_result = []
     for news in db_result:
         search_result.append((news["title"], news["url"]))
     return search_result
+
 
 def search_by_date(date):
     """Seu código deve vir aqui"""
@@ -18,5 +21,3 @@ def search_by_source(source):
 
 def search_by_category(category):
     """Seu código deve vir aqui"""
-
-search_by_title("vamos")

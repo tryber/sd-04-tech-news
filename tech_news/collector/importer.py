8ffef1e6a11af4b6efcbb5c451c3794fb528123c
lucasfranca90 <luc.rangel90@gmail.com>
2021-01-28 15:56:43 -0300

    testando pr

--- tech_news/collector/importer.py
+++
@@ -1,2 +0,0 @@
-def csv_importer(filepath):
-    """Seu código deve vir aqui"""
--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+from parsel import Selector
 
 def fetch_content(url, timeout=3, delay=0.5):
     sleep(delay)
@@ -13,5 +14,57 @@ def fetch_content(url, timeout=3, delay=0.5):
     else:
         return response.text
 
+# def data_scrape(url, fetcher):
+#     selector = Selector(text=fetcher(url))
+#     title = selector.css("#js-article-title::text").get()
+#     timestamp = selector.css(".tec--timestamp__item time::attr(datetime)").get()
+#     writer = selector.css(".tec--author__info__link::text").get()
+#     shares_count = selector.css("button::attr(data-count)").get()
+#     comments_count = selector.css(".tec--btn::attr(data-count)").get()
+#     summary = selector.css(".tec--article__body > p::text").get()
+#     sources = selector.css(".z--mb-16 > div a::text").getall()
+#     categories = selector.css("#js-categories a::text").getall()
+#     return {
+#         "url": url,
+#         "title": title,
+#         "timestamp": timestamp,
+#         "writer": writer,
+#         "shares_count": int(shares_count),
+#         "comments_count": int(comments_count),
+#         "summary": summary,
+#         "sources": sources,
+#         "categories": categories,
+#     }
+
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    data_list = []
+    base_url = "https://www.tecmundo.com.br/novidades"
+    selector = Selector(fetcher(base_url))
+    items = selector.css(".tec--card__info h3 a::attr(href)").getall()
+
+    for item in items:
+        content = fetcher(item)
+        selector = Selector(text=content)
+        title = selector.css("#js-article-title::text").get()
+        writer = selector.css(".tec--author__info__link::text").get()
+        shares_count = selector.css("button::attr(data-count)").get()
+        comments_count = selector.css(".tec--btn::attr(data-count)").get()
+        timestamp = selector.css(".tec--timestamp__item time::attr(datetime)").get()
+        summary = selector.css(".tec--article__body > p::text").get()
+        sources = selector.css(".z--mb-16 > div a::text").getall()
+        categories = selector.css("#js-categories a::text").getall()
+
+        data_list.append(
+            {
+                "url": item,
+                "title": title,
+                "timestamp": timestamp,
+                "writer": writer,
+                "shares_count": int(shares_count),
+                "comments_count": int(comments_count),
+                "summary": summary,
+                "sources": sources,
+                "categories": categories,
+            }
+        )
+    return data_list

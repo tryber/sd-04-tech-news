3095c8abbe13f7406e27681b56170d709676ff39
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-01-27 23:55:48 -0300

    completed requirement 5

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,12 @@
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+from tech_news import database
 
+
+def search_by_title(title):
+    db_result = database.search_news({"title": {"$regex": title, "$options": "i"}})
+    search_result = []
+    for news in db_result:
+        search_result.append((news["title"], news["url"]))
+    return search_result
 
 def search_by_date(date):
     """Seu código deve vir aqui"""
@@ -12,3 +18,5 @@ def search_by_source(source):
 
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+search_by_title("vamos")

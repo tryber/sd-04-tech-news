5cf6012e94c6ec1dca66c860d3307ad0bd39495b
danilossousa <danilosantossousa@gmail.com>
2021-01-28 13:37:07 -0300

    v1.0.1

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,54 @@
+import csv
+from pymongo import MongoClient
+from decouple import config
+
+DB_HOST = config("DB_HOST", default="localhost")
+DB_PORT = config("DB_PORT", default="27017")
+
+client = MongoClient(host=DB_HOST, port=int(DB_PORT))
+
+db = client.tech_news
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    with open(filepath, "w") as file:
+        writeCsv = csv.writer(file, delimiter=";")
+        headers = [
+            "url",
+            "title",
+            "timestamp",
+            "writer",
+            "shares_count",
+            "comments_count",
+            "summary",
+            "sources",
+            "categories",
+        ]
+        writeCsv.writerow(headers)
+        for document in db.news.find({}):
+            url = document["url"]
+            title = document["title"]
+            timestamp = document["timestamp"]
+            writer = document["writer"]
+            shares_count = document["shares_count"]
+            comments_count = document["comments_count"]
+            summary = document["summary"]
+            sources = "".join(document["sources"])
+            categories = ",".join(document["categories"])
+            writeCsv.writerow(
+                [
+                    url,
+                    title,
+                    timestamp,
+                    writer,
+                    shares_count,
+                    comments_count,
+                    summary,
+                    sources,
+                    categories,
+                ]
+            )
+
+
+csv_exporter("news.csv")

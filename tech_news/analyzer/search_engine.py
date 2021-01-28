c6ed57c249c3888541b6133479b32b3b3578ee22
mirellaalves <mirella.alves@gmail.com>
2021-01-28 12:50:04 -0300

    requirement 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -12,11 +12,20 @@ def search_by_title(title):
             )
 
     return news_list_by_title
-# source: https://docs.mongodb.com/manual/reference/operator/query/regex/
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        news_list_by_date = []
+        news_date = database.search_news({"timestamp": {"$regex": date}})
+        if len(news_date) == 1:
+            news_list_by_date.append(
+                (news_date[0]["title"], news_date[0]["url"])
+                )
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    return news_list_by_date
 
 
 def search_by_source(source):
@@ -25,3 +34,6 @@ def search_by_source(source):
 
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+# mongo regex source:
+# https://docs.mongodb.com/manual/reference/operator/query/regex/

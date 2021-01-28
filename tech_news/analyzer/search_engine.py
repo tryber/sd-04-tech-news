516c4f53ec827527c50c95419978b3f6732ea4b0
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-28 08:45:08 -0300

    fix

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,66 +1,44 @@
-from pymongo import MongoClient
-import re
-import datetime
-
-# para testes locais com usuário e senha
-client = MongoClient(
-    "mongodb://root:rootteste@localhost:27017/?authMechanism=DEFAULT"
-)
-db = client.tech_news
-
-""" 5 """
+from tech_news import database
+from datetime import datetime
 
 
 def search_by_title(title):
-    ar = []
-    for document in db.news.find(
-        {"title": {"$regex": title, "$options": "-i"}}
-    ):
-        title = document["title"]
-        url = document["url"]
-        ar.append((title, url))
-    return ar
-
-
-""" 6 """
+    db_result = database.search_news(
+        {"title": {"$regex": title, "$options": "i"}}
+    )
+    search_result = []
+    for news in db_result:
+        search_result.append((news["title"], news["url"]))
+    return search_result
 
 
 def search_by_date(date):
     try:
-        datetime.datetime.strptime(date, "%Y-%m-%d")
+        datetime.strptime(date, "%Y-%m-%d")
+        db_result = database.search_news({"timestamp": {"$regex": date}})
+        search_result = []
+        for news in db_result:
+            search_result.append((news["title"], news["url"]))
+        return search_result
     except ValueError:
         raise ValueError("Data inválida")
-    else:
-        regex = re.compile(date, re.IGNORECASE)
-        with client as session:
-            db = session.tech_news
-            query = db.news.find(
-                {"timestamp": regex}, {"title": 1, "url": 1, "_id": 0}
-            )
-            result = [(doc["title"], doc["url"]) for doc in query]
-            print(result)
-            if not len(result):
-                return []
-            return result
-
-
-""" 7 """
 
 
 def search_by_source(source):
-    results = db.news.find(
-        {"sources": {"$regex": re.compile(source, re.IGNORECASE)}},
-        {"title": True, "_id": False, "url": True},
+    db_result = database.search_news(
+        {"sources": {"$regex": source, "$options": "i"}}
     )
-    return [(result["title"], result["url"]) for result in results]
-
-
-""" 8 """
+    search_result = []
+    for news in db_result:
+        search_result.append((news["title"], news["url"]))
+    return search_result
 
 
 def search_by_category(category):
-    results = db.news.find(
-        {"categories": {"$regex": re.compile(category, re.IGNORECASE)}},
-        {"title": True, "_id": False, "url": True},
+    db_result = database.search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
     )
-    return [(result["title"], result["url"]) for result in results]
+    search_result = []
+    for news in db_result:
+        search_result.append((news["title"], news["url"]))
+    return search_result

91021e1261b8de4a64cabc2ceb6f928d0150bce0
Thabata <dornelasthabata@gmail.com>
2021-01-28 11:48:04 -0300

    tentativa voltando branch

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,57 @@
+import csv
+# pip install python-csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    try:
+        with open(filepath) as file:
+            # https://docs.python.org/3/library/csv.html#csv.DictReader
+            read_content = csv.DictReader(file, delimiter=";")
+            content = ""
+            for content in read_content:
+                values = content
+                return [values]
+    except FileNotFoundError:
+        raise ValueError("Arquivo tests/file_not_exist.csv não encontrado")
+
+
+'''
+def headerForm(header):
+    if (
+        "url"
+        and "title"
+        and "timestamp"
+        and "writer"
+        and "shares_count"
+        and "comments_count"
+        and "summary"
+        and "sources"
+        and "categories" not in header
+    ):
+        raise ValueError("Formato invalido")
+def format_object(data, header):
+    csv_list = []
+    for info in data:
+        new_obj = {}
+        for i in range(len(header)):
+            key = header[i]
+            new_obj[key] = info[i]
+        csv_list.append(new_obj)
+    return csv_list
+def csv_importer(filepath):
+    csv_list = []
+    try:
+        if not filepath.endswith(".csv"):
+            raise ValueError("Formato invalido")
+        with open(filepath, "r") as file:
+            content = csv.reader(file, delimiter=";", quotechar='"')
+            header, *data = content
+            headerForm(header)
+            csv_list = format_object(data, header)
+    except FileNotFoundError:
+        raise ValueError("Arquivo file_not_exist.csv não encontrado")
+    else:
+        return csv_list
+'''
--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,59 @@
+import requests
+import time
+from time import sleep
+from parsel import Selector
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    time.sleep(delay)
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()  # retorna 1 objeto HTTPError
+        sleep(delay)
+    except (requests.exceptions.HTTPError, requests.exceptions.ReadTimeout):
+        return ""
+    else:
+        return response.text
 
 
-def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+def format_new_object(url, selector):
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    writer = selector.css(".tec--author__info__link::text").get()
+    shares_count = selector.css(".tec--toolbar__item::text").re_first(r"\d")
+    if shares_count is None:
+        shares_count = "0"
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    if comments_count is None:
+        comments_count = "0"
+    summary = selector.css(".tec--article__body *::text").get()
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("#js-categories .tec--badge::text").getall()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+
+
+def scrape(fetcher=fetch_content, pages=1):
+    news_list = []
+    for page in range(1, pages + 1):
+        response_news = fetcher(
+            f"https://www.tecmundo.com.br/novidades?page={page}"
+        )
+        selector_news = Selector(text=response_news)
+        for new in selector_news.css(
+            ".tec--list__item .tec--card__title__link::attr(href)"
+        ).getall():
+            response_new = fetcher(new)
+            selector_new = Selector(text=response_new)
+            new_obj = format_new_object(new, selector_new)
+            news_list.append(new_obj)
+    return news_list

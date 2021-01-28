dbc91adc1dbf4643906285827e18991076c5e847
mirellaalves <mirella.alves@gmail.com>
2021-01-28 12:02:32 -0300

    requirement 4

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,37 @@
+import csv
+from tech_news.database import find_news
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+
+    with open(filepath, "w") as file:
+        writer = csv.writer(file, delimiter=";")
+        headers = [
+            "url",
+            "title",
+            "timestamp",
+            "writer",
+            "shares_count",
+            "comments_count",
+            "summary",
+            "sources",
+            "categories",
+        ]
+        writer.writerow(headers)
+
+        news = find_news()
+
+        for row in news:
+            writer.writerow([
+                row["url"],
+                row["title"],
+                row["timestamp"],
+                row["writer"],
+                row["shares_count"],
+                row["comments_count"],
+                row["summary"],
+                ','.join(row["sources"]),
+                ','.join(row["categories"]),
+                ])

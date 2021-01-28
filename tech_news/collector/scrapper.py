c243cf1bc8f1d0f75ca2295769f8da568884eecd
lucasfranca90 <luc.rangel90@gmail.com>
2021-01-28 16:28:49 -0300

    lintado denovo

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -17,29 +17,6 @@ def fetch_content(url, timeout=3, delay=0.5):
         return response.text
 
 
-# def data_scrape(url, fetcher):
-#     selector = Selector(text=fetcher(url))
-#     title = selector.css("#js-article-title::text").get()
-#     timestamp = selector.css(".tec--timestamp__item time::attr(datetime)").get()
-#     writer = selector.css(".tec--author__info__link::text").get()
-#     shares_count = selector.css("button::attr(data-count)").get()
-#     comments_count = selector.css(".tec--btn::attr(data-count)").get()
-#     summary = selector.css(".tec--article__body > p::text").get()
-#     sources = selector.css(".z--mb-16 > div a::text").getall()
-#     categories = selector.css("#js-categories a::text").getall()
-#     return {
-#         "url": url,
-#         "title": title,
-#         "timestamp": timestamp,
-#         "writer": writer,
-#         "shares_count": int(shares_count),
-#         "comments_count": int(comments_count),
-#         "summary": summary,
-#         "sources": sources,
-#         "categories": categories,
-#     }
-
-
 def scrape(fetcher, pages=1):
     data_list = []
     base_url = "https://www.tecmundo.com.br/novidades"

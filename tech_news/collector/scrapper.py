6ff46aa58f5221d62e1dde37d740180823139de8
caiolmf <caio.lmf@gmail.com>
2021-01-28 09:46:52 -0300

    lint

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -8,10 +8,10 @@ def fetch_content(url, timeout=3, delay=0.5):
         print(response)
         sleep(delay)
         print(response.status_code)
-        
+
         if response.status_code != 200:
             return ""
-        
+
         return response.text
     except requests.ReadTimeout:
         return ""

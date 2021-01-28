d0c2461f69bea2a8bd228f3d3c268d8c6a520446
Victor L <victor.lustosacp@gmail.com>
2021-01-28 19:36:17 -0300

    saida pro req 2

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -15,8 +15,18 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    # selector = Selector(text = fetch_content
-    # ("https://www.tecmundo.com.br/novidades"))
-    # url = selector.css("figure.tec--card__thumb a::attr(href)").getall()
-    # print(url)
-    
+    url = "https://www.tecmundo.com.br/novidades?page="
+    list_news = []
+    for page in range(1, pages + 1):
+        selector = Selector(text=fetcher(url + str(page)))
+        for news in selector.css("h3.tec--card__title"):
+            url = news.css("a::attr(href)").get()
+            details_url = Selector(text=fetcher(url))
+            title = details_url.css("#js-article-title::text").get()
+            timestamp = details_url.css("time::attr(datetime)").get()
+            writer = details_url.css(".z--m-none strong").get()
+            page_details = {"url": url, "title": title,
+                            "timestamp": timestamp, "writer": writer}
+
+            list_news.append(page_details)
+    print(list_news)

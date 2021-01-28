172535f867612d8f094e121fdb0b61c486b7046b
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-28 08:17:19 -0300

    5th

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,21 @@
+from pymongo import MongoClient
+
+# para testes locais com usuário e senha
+client = MongoClient(
+    "mongodb://root:rootteste@localhost:27017/?authMechanism=DEFAULT"
+)
+db = client.tech_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    ar = []
+    for document in db.news.find(
+        {"title": {"$regex": title, "$options": "-i"}}
+    ):
+        title = document["title"]
+        url = document["url"]
+        ar.append((title, url))
+    return ar
 
 
 def search_by_date(date):

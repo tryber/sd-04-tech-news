e3ddaac9bacf01bbdd8b67863094bc69dad61fea
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-01-28 00:20:49 -0300

    completed requirement 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news import database
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -12,7 +13,15 @@ def search_by_title(title):
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        db_result = database.search_news({"timestamp": {"$regex": date}})
+        search_result = []
+        for news in db_result:
+            search_result.append((news["title"], news["url"]))
+        return search_result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 def search_by_source(source):

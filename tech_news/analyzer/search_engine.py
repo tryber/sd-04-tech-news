48904a8603e794c6c28e491525229294796704fe
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-28 08:26:47 -0300

    Up to 8th req

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
+import re
+import datetime
 
 # para testes locais com usuário e senha
 client = MongoClient(
     "mongodb://root:rootteste@localhost:27017/?authMechanism=DEFAULT"
 )
 db = client.tech_news
+
+""" 5 """
 
 
 def search_by_title(title):
@@ -18,13 +22,45 @@ def search_by_title(title):
     return ar
 
 
+""" 6 """
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        regex = re.compile(date, re.IGNORECASE)
+        with client as session:
+            db = session.tech_news
+            query = db.news.find(
+                {"timestamp": regex}, {"title": 1, "url": 1, "_id": 0}
+            )
+            result = [(doc["title"], doc["url"]) for doc in query]
+            print(result)
+            if not len(result):
+                return []
+            return result
+
+
+""" 7 """
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    results = db.news.find(
+        {"sources": {"$regex": re.compile(source, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True},
+    )
+    return [(result["title"], result["url"]) for result in results]
+
+
+""" 8 """
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    results = db.news.find(
+        {"categories": {"$regex": re.compile(category, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True},
+    )
+    return [(result["title"], result["url"]) for result in results]

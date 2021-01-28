fe3e2e64ef5f2b26cf488d9feffd4d91a19700f5
caiolmf <caio.lmf@gmail.com>
2021-01-28 09:45:54 -0300

    req: do req 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,20 @@
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        print(response)
+        sleep(delay)
+        print(response.status_code)
+        
+        if response.status_code != 200:
+            return ""
+        
+        return response.text
+    except requests.ReadTimeout:
+        return ""
 
 
 def scrape(fetcher, pages=1):

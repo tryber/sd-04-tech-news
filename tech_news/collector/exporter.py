dfd2d58a687ede8a65899629f33568b330e22265
Mateus-Moreira <ashuraw@hotmail.com>
2021-01-28 13:03:15 -0300

    req 4

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,30 @@
+import csv
+from tech_news.database import find_news
+
+header = [
+    "url",
+    "title",
+    "timestamp",
+    "writer",
+    "shares_count",
+    "comments_count",
+    "summary",
+    "sources",
+    "categories",
+]
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+
+    with open(filepath, "w", newline="", encoding="utf-8") as file:
+        whriteCsv = csv.DictWriter(file, fieldnames=header, delimiter=";")
+        findNews = find_news()
+
+        for field in findNews:
+            for key in field:
+                if type(field[key]) == list:
+                    field[key] = ",".join(field[key])
+
+        whriteCsv.writeheader()
+        whriteCsv.writerows(findNews)

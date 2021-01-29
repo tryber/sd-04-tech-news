7547dffeef70a226f0d195264e8a7182610ffd50
leandromarten1 <leandromarten1@gmail.com>
2021-01-29 18:41:58 -0300

    Fix Req 2

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,40 @@
+import csv
+
+
+def converter_object(data, header):
+    lista = []
+    for row in data:
+        item = {}
+        for index in range(len(header)):
+            item[header[index]] = item[index]
+        lista.append(item)
+
+    return lista
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("formato inválido")
+    try:
+        with open(filepath, "r") as file:
+            header_file = [
+                "url",
+                "title",
+                "timestamp",
+                "writer",
+                "shares_count",
+                "comments_count",
+                "summary",
+                "sources",
+                "categories",
+            ]
+
+            header, *data = csv.reader(file, delimiter=";")
+
+            if header != header_file:
+                raise ValueError("formato inválido")
+
+            return converter_object(data, header)
+
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -9,12 +9,11 @@ def fetch_content(url, timeout=3, delay=0.5):
     try:
         response = requests.get(url, timeout=timeout)
         response.raise_for_status()
-    except (requests.ReadTimeout, requests.HTTPError):
+    except (requests.ReadTimeout, requests.exceptions.HTTPError):
         return ""
     else:
-        return response.text
-    finally:
         time.sleep(delay)
+        return response.text
 
 
 def scrape(fetcher, pages=1):
@@ -24,28 +23,34 @@ def scrape(fetcher, pages=1):
         response = fetcher(BASE_URL + "?page={count}")
         selector = Selector(text=response)
 
-        for url in selector.css(".tec--card__title a::attr(href)").getall():
+        for url in selector.css(".tec--list__item h3 a::attr(href)").getall():
             selector_url = Selector(text=fetcher(url))
             list_news.append(
                 {
                     "url": url,
-                    "title": selector_url.css("#js-article-title::text").get(),
+                    "title": selector_url.css(
+                        ".tec--article__header__title::text"
+                    ).get(),
                     "timestamp": selector_url.css(
                         ".tec--timestamp__item time::attr(datetime)"
                     ).get(),
                     "writer": selector_url.css(
                         ".tec--author__info__link::text"
                     ).get(),
-                    "shares_count": selector_url.css(
-                        ".tec--toolbar__item::text"
-                    ).re_first(r"\d+"),
-                    "comments_count": selector_url.css(
-                        "#js-comments-btn::text"
-                    ).re_first(r"\d+"),
+                    "shares_count": int(
+                        selector_url.css(".tec--toolbar__item::text").re_first(
+                            r"[0-9]+"
+                        )
+                    ),
+                    "comments_count": int(
+                        selector_url.css("#js-comments-btn::text").re_first(
+                            r"[0-9]+"
+                        )
+                    ),
                     "summary": selector_url.css(
                         ".tec--article__body *::text"
                     ).get(),
-                    "sources": selector_url.css(".tec--badge::text").getall(),
+                    "sources": selector_url.css(".z--mb-16 a::text").getall(),
                     "categories": selector_url.css(
                         "#js-categories a::text"
                     ).getall(),

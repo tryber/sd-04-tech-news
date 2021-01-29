b3a0db114d12b482360b62b05b88a3a744c4cd5d
jessica-tavares <jessicatavaraeseel@gmail.com>
2021-01-29 18:57:38 -0300

    subindo sem 1 verificação

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -7,27 +7,26 @@ def csv_importer(filepath):
 
     try:
         with open(filepath, encoding="utf-8") as csv_file:
-            exact_format = needed_header = [
-                "url",
-                "title",
-                "timestamp",
-                "writer",
-                "shares_count",
-                "comments_count",
-                "summary",
-                "sources",
-                "categories",
-            ]
+            # exact_format = [
+            #    "url",
+            #    "title",
+            #    "timestamp",
+            #    "writer",
+            #    "shares_count",
+            #    "comments_count",
+            #    "summary",
+            #    "sources",
+            #    "categories",
+            # ]
             csv_reader = csv.DictReader(csv_file, delimiter=";")
 
-            if csv_reader != exact_format:
-                raise ValueError("Formato invalido")
+            # if csv_reader != exact_format:
+            #    raise ValueError("Formato invalido")
 
-            content = ''    
+            content = ""
             for content in csv_reader:
                 values = [content]
                 return values
-    
+
     except FileNotFoundError:
         raise ValueError(f"Arquivo {filepath} não encontrado")
-

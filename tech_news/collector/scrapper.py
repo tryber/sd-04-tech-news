371e195e148104d7227ae8ff30c30d76ac85cf3d
Josias Viveiro <josiasviveiro@gmail.com>
2021-01-29 14:58:31 -0300

    Req. 1 - incompleto

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,7 +1,20 @@
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
-    print('Iniciando o projeto tech-news')
-    print('fetch_content, URL: ', url)
+    response = requests.get(url, timeout=timeout)
+    sleep(delay)
+    # print('Iniciando o projeto tech-news')
+    # print('response:', response)
+    # print(response.status_code)
+    if (response.status_code != 200):
+        response = ""
+        return response
+    # print('headers:', response.headers["Content-Type"])
+    # print('response.text:', response.text)
+    return response.text
 
 
 def scrape(fetcher, pages=1):

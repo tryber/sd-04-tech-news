b1184efec1595ba5a355c297f1fd042ac254b6e8
Ronan-Fernandes <ronanf.sk@gmail.com>
2021-01-28 22:07:59 -0300

    menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,5 +1,21 @@
 def collector_menu():
-    print("valew cassio")
+    entrada = input(
+        "Selecione uma das opções a seguir:\n"
+        " 1 - Importar notícias a partir de um arquivo CSV;\n"
+        " 2 - Exportar notícias para CSV;\n"
+        " 3 - Raspar notícias online;\n 4 - Sair."
+    )
+
+    if entrada == "1":
+        pass
+    elif entrada == "2":
+        pass
+    elif entrada == "3":
+        pass
+    elif entrada == "4":
+        print("Encerrando script\n")
+    else:
+        print("Opção inválida\n")
 
 
 def analyzer_menu():

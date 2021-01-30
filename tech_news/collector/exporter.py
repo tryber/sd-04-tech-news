d31461cbb92a3b929e5ac14ae9d0d9a1df21d766
jessica-tavares <jessicatavaraeseel@gmail.com>
2021-01-30 10:27:44 -0300

    mudanças req 4

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,39 @@
+import csv
+from tech_news.database import find_news
+
+exact_format = [
+    "url",
+    "title",
+    "timestamp",
+    "writer",
+    "shares_count",
+    "comments_count",
+    "summary",
+    "sources",
+    "categories",
+]
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+
+    with open(filepath, "w") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow(exact_format)
+        news = find_news()
+
+        for newNews in news:
+            writer.writerow(
+                [
+                    newNews["url"],
+                    newNews["title"],
+                    newNews["timestamp"],
+                    newNews["writer"],
+                    newNews["shares_count"],
+                    newNews["comments_count"],
+                    newNews["summary"],
+                    ','.join(newNews["sources"]),
+                    ','.join(newNews["categories"]),
+                ]
+            )

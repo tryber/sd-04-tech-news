b73a4f9287152734f67024f17d32ee6c285495a1
jessica-tavares <jessicatavaraeseel@gmail.com>
2021-01-30 11:30:04 -0300

    subindo mais um sem teste local... :/

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,38 @@
+from tech_news.database import aggregate_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = []
+    search = aggregate_news(
+        [
+            {
+                "$addFields": {
+                    "popularity": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {"$sort": {"popularity": -1, "title": 1}},
+            {"$limit": 5},
+        ]
+    )
+    for new in search:
+        result.append((new["title"], new["url"]))
+    return result
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    result = []
+    search = aggregate_news(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "quantity": {"$sum": 1}}},
+            {"$sort": {"quantity": -1, "_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+
+    for news in search:
+        result.append((news["_id"]))
+
+    return result

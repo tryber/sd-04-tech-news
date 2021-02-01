8dbe86f548bc60f02b930fea03bfd24101d4d313
leandromarten1 <leandromarten1@gmail.com>
2021-02-01 18:23:13 -0300

    requisito 3 com dictReader

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,40 +1,15 @@
 import csv
 
 
-def converter_object(data, header):
-    lista = []
-    for row in data:
-        item = {}
-        for index in range(len(header)):
-            item[header[index]] = item[index]
-        lista.append(item)
-
-    return lista
-
-
 def csv_importer(filepath):
     if not filepath.endswith(".csv"):
-        raise ValueError("formato inválido")
+        raise ValueError("Formato invalido")
     try:
-        with open(filepath, "r") as file:
-            header_file = [
-                "url",
-                "title",
-                "timestamp",
-                "writer",
-                "shares_count",
-                "comments_count",
-                "summary",
-                "sources",
-                "categories",
-            ]
-
-            header, *data = csv.reader(file, delimiter=";")
-
-            if header != header_file:
-                raise ValueError("formato inválido")
-
-            return converter_object(data, header)
-
+        with open(filepath, 'r') as file:
+            data = csv.DictReader(file, delimiter=";")
+            row = ""
+            for row in data:
+                result = row
+                return [result]
     except FileNotFoundError:
         raise ValueError(f"Arquivo {filepath} não encontrado")

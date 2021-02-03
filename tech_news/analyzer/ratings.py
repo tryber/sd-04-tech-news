c1e025b83631146adcad769399b8d6dab9e2c228
Victor L <victor.lustosacp@gmail.com>
2021-02-03 09:37:00 -0300

    test2

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,13 @@
+from ..database import get_top5, get_top_categories
+
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    result = get_top5()
+    return [(news["title"], news["url"]) for news in result]
 
 
 def top_5_categories():
     """Seu código deve vir aqui"""
+    result = get_top_categories()
+    return [cat["_id"] for cat in result]

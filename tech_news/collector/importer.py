abd233a68265e76316ed6908397bd22cbbc45043
pedrotpo <pedrotpoliveira@gmail.com>
2021-02-05 17:42:44 -0300

    Req 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,30 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    try:
+        with open(filepath) as file:
+            header, *news = csv.reader(file, delimiter=";")
+            expected_header = [
+                "url",
+                "title",
+                "timestamp",
+                "writer",
+                "shares_count",
+                "comments_count",
+                "summary",
+                "sources",
+                "categories",
+            ]
+
+            if header != expected_header:
+                raise ValueError("Formato inválido")
+            formatted_news = [
+                {header[i]: article[i] for i in range(len(article))}
+                for article in news
+            ]
+            return formatted_news
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")

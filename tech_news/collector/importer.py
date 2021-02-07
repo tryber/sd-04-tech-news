3e16f5a5c9fbce53273892e1e085a1713aa6449d
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-02-07 19:08:50 -0300

    req3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,17 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("invalid format")
+    try:
+        csv_file = open(filepath)
+    except FileNotFoundError:
+        return  ValueError("file not found")
+    else:
+        with open(filepath) as file:
+            read_content = csv.DictReader(file, delimiter=";")
+            for content in read_content:
+                print(content)
+                values = content
+                return [values]

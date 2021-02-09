7ec047b8b7c92be98d53c1c5e2e514e210012100
leandromarten1 <leandromarten1@gmail.com>
2021-02-09 17:18:11 -0300

    Req Bonus 12

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,22 +1,30 @@
+from tech_news.collector.importer import csv_importer
+from tech_news.collector.exporter import csv_exporter
+from tech_news.collector.scrapper import scrape, fetch_content
 import sys
 
 
 def collector_menu():
-    print("""Selecione uma das opções a seguir:
+    print(
+        """Selecione uma das opções a seguir:
  1 - Importar notícias a partir de um arquivo CSV;
  2 - Exportar notícias para CSV;
  3 - Raspar notícias online;
- 4 - Sair.""")
+ 4 - Sair."""
+    )
 
     # todo input é string, então transofrmo para integer
     user_choice = int(input())
 
     if user_choice == 1:
-        input("Digite o nome do arquivo CSV a ser importado:")
+        file = input("Digite o nome do arquivo CSV a ser importado:")
+        action = csv_importer(file)
     elif user_choice == 2:
-        input("Digite o nome do arquivo CSV a ser exportado:")
+        file = input("Digite o nome do arquivo CSV a ser exportado:")
+        return csv_exporter(file)
     elif user_choice == 3:
-        input("Digite a quantidade de páginas a serem raspadas:")
+        pages = int(input("Digite a quantidade de páginas a serem raspadas:"))
+        action = scrape(fetcher=fetch_content, pages=pages)
     elif user_choice == 4:
         return print("Encerrando script")
     else:

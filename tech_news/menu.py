8459a1658b3b1bc50d42782e816f068377a8203b
leandromarten1 <leandromarten1@gmail.com>
2021-02-09 19:15:51 -0300

    Fix flake

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -70,7 +70,16 @@ def option_seven():
 
 
 def analyzer_menu():
-    print("Selecione uma das opções a seguir:\n 1 - Buscar notícias por título;\n 2 - Buscar notícias por data;\n 3 - Buscar notícias por fonte;\n 4 - Buscar notícias por categoria;\n 5 - Listar top 5 notícias;\n 6 - Listar top 5 categorias;\n 7 - Sair.")
+    print(
+        "Selecione uma das opções a seguir:\n "
+        "1 - Buscar notícias por título;\n "
+        "2 - Buscar notícias por data;\n "
+        "3 - Buscar notícias por fonte;\n "
+        "4 - Buscar notícias por categoria;\n "
+        "5 - Listar top 5 notícias;\n "
+        "6 - Listar top 5 categorias;\n "
+        "7 - Sair.\n "
+    )
 
     user_choice = int(input())
 

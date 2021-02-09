9c5d0cb131c952e24096f128fc668d0484ec1ba4
leandromarten1 <leandromarten1@gmail.com>
2021-02-09 19:11:15 -0300

    Done?

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -2,6 +2,13 @@ from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.scrapper import scrape, fetch_content
 from tech_news.database import create_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_source,
+    search_by_category,
+)
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
 import sys
 
 
@@ -38,5 +45,46 @@ def collector_menu():
         return print("Opção inválida", file=sys.stderr)
 
 
+# REFERENCIA PARA REDUZIR A COMPLEXIDADE DOS IFS
+# https://jaxenter.com/implement-switch-case-statement-python-138315.html
+
+
+def option_one():
+    return search_by_title(input("Digite o título:"))
+
+
+def option_two():
+    return search_by_date(input("Digite a data no formato aaaa-mm-dd:"))
+
+
+def option_three():
+    return search_by_source(input("Digite a fonte:"))
+
+
+def option_four():
+    return search_by_category(input("Digite a categoria:"))
+
+
+def option_seven():
+    print("Encerrando script")
+
+
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    print("Selecione uma das opções a seguir:\n 1 - Buscar notícias por título;\n 2 - Buscar notícias por data;\n 3 - Buscar notícias por fonte;\n 4 - Buscar notícias por categoria;\n 5 - Listar top 5 notícias;\n 6 - Listar top 5 categorias;\n 7 - Sair.")
+
+    user_choice = int(input())
+
+    options = {
+        1: option_one,
+        2: option_two,
+        3: option_three,
+        4: option_four,
+        5: top_5_news,
+        6: top_5_categories,
+        7: option_seven,
+    }
+
+    try:
+        return options[user_choice]()
+    except KeyError:
+        print("Opção inválida", file=sys.stderr)

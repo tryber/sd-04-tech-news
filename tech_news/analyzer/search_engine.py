d05eb96125b1672c481fc785ada9bc4457547b99
leandromarten1 <leandromarten1@gmail.com>
2021-02-09 15:35:12 -0300

    Requisitos 6 e 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from tech_news.database import search_news
 import re
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -18,11 +19,31 @@ def search_by_title(title):
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # https://docs.python.org/pt-br/3/library/datetime.html#strftime-and-strptime-behavior
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+
+        result = []
+        search_results = search_news({"timestamp": {"$regex": date}})
+        for news in search_results:
+            result.append((news["title"], news["url"]))
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        return result
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    result = []
+    search_results = search_news(
+        {"sources": {"$all": [re.compile(source, re.IGNORECASE)]}}
+    )
+
+    for news in search_results:
+        result.append((news["title"], news["url"]))
+
+    # print(result)
+    return result
 
 
 def search_by_category(category):

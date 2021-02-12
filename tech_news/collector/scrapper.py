de4cc7b13e093dc93642bed1987aade1eed64066
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-12 20:06:52 -0300

    Req.2 - scrape - Começando

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+from parsel import Selector
 
 
 def fetch_content(url, timeout=3, delay=0.5):
@@ -21,4 +22,25 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    news = []
+    base_url = "https://www.tecmundo.com.br/novidades?page=" 
+    response = fetcher(f"{base_url}{pages}")
+    print("\nRESPONSE:", response)
+    selector = Selector(text=response)
+    url = selector.css(".tec--card__title a::attr(href)").getall()
+    title = selector.css(".tec--card__title a::text").getall()
+    #timestamp = selector.css("").getall()
+    print("\nURL:", url[0])
+    print("\nTITLE:", title[0])
+    print("\nTIMESTAMP:", title[0])
+
+    # news.append(new)
+    # url = selector.css(".tec--list").getall()
+    print("\nSELECTOR:", selector)
+    # print(url)
+    return selector
+
+
+# Teste Local
+# python3 -i tech_news/collector/scrapper.py
+# scrape(fetcher=fetch_content, pages=2)

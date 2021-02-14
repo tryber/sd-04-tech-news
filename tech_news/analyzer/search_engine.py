edb71186b8cde7597fc530fcef692b13213796fb
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-02-14 12:27:10 -0300

    search engine req

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,14 +1,39 @@
+from tech_news.database import find_news
+from datetime import datetime
+
+
+def create_tuple(data):
+    if data == []:
+        return []
+    search_for_news = []
+    for news in data:
+        search_for_news.append((news["title"], news["url"]))
+        return search_for_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_data = search_news({"title": {"$regex": title, "$options": "i"}})
+    return create_tuple(news_data)
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        if datetime.strptime(date, "%Y-%m-%d"):
+            news_data = search_news(
+                {"timestamp": {"$regex": date, "$options": "i"}}
+            )
+            return create_tuple(news_data)
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news_data = search_news({"sources": {"$regex": source, "$options": "i"}})
+    return create_tuple(news_data)
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news_data = search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
+    )
+    return create_tuple(news_data)

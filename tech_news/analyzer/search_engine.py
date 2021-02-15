5b7fbdd30cc0b9d7b1b01d9ffa77023ce0fbb89f
pedrotpo <pedrotpoliveira@gmail.com>
2021-02-15 07:40:21 -0300

    Req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,15 +1,26 @@
 from tech_news.database import search_news
+import datetime
+
+
+def general_search(field, query):
+    result = search_news({field: query})
+    return [(news["title"], news["url"]) for news in result]
 
 
 def search_by_title(title):
-    data = search_news({"title": {"$regex": title, "$options": "i"}})
+    data = general_search("title", title)
     if data == []:
         return []
-    return [(news["title"], news["url"]) for news in data]
+    return data
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+        data = general_search("timestamp", date)
+        return data
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 def search_by_source(source):

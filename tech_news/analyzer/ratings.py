b641a184315676a0a77d9bc49ab98367e4712ec6
pedrotpo <pedrotpoliveira@gmail.com>
2021-02-15 07:52:39 -0300

    Req 9 e 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,30 @@
+from tech_news.database import search_aggregator
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    data = search_aggregator(
+        [
+            {
+                "$addFields": {
+                    "popularity": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {"$sort": {"popularity": -1, "title": 1}},
+            {"$limit": 5},
+        ]
+    )
+    return [(news["title"], news["url"]) for news in data]
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    data = search_aggregator(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "total": {"$sum": 1}}},
+            {"$sort": {"total": -1, "_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+    return [(news["title"], news["url"]) for news in data]

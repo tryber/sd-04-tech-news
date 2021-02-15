7b3621074d95d98f493ad0c91e2bc97af20f0fe6
pedrotpo <pedrotpoliveira@gmail.com>
2021-02-15 09:04:20 -0300

    Menu Req

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -2,13 +2,14 @@ from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.scrapper import scrape, fetch_content
 from tech_news.database import create_news
-from tech_news.analyzer.ratings import top_5_news, top_5_categories
-from tech_news.analyzer.search_engine import (
-    search_by_title,
-    search_by_date,
-    search_by_source,
-    search_by_category,
-)
+
+# from tech_news.analyzer.ratings import top_5_news, top_5_categories
+# from tech_news.analyzer.search_engine import (
+#     search_by_title,
+#     search_by_date,
+#     search_by_source,
+#     search_by_category,
+# )
 import sys
 
 

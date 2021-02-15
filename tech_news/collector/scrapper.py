56727c885a26895118b09c9578f93527415e9ebb
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-15 17:00:52 -0300

    scrapper.py - pega alguns atributos de uma notícia (incompleto)

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -10,33 +10,62 @@ def fetch_content(url, timeout=3, delay=0.5):
     except requests.ReadTimeout:
         response = ""
         return response
-    # print('Iniciando o projeto tech-news')
-    # print('response:', response)
-    # print(response.status_code)
-    if (response.status_code != 200):
+
+    if response.status_code != 200:
         response = ""
         return response
-    # print('headers:', response.headers["Content-Type"])
-    # print('response.text:', response.text)
+
     return response.text
 
 
 def scrape(fetcher, pages=1):
     news = []
-    base_url = "https://www.tecmundo.com.br/novidades?page=" 
+    base_url = "https://www.tecmundo.com.br/novidades?page="
     response = fetcher(f"{base_url}{pages}")
-    print("\nRESPONSE:", response)
+    # print("\nRESPONSE:", response)
     selector = Selector(text=response)
-    url = selector.css(".tec--card__title a::attr(href)").getall()
-    title = selector.css(".tec--card__title a::text").getall()
-    #timestamp = selector.css("").getall()
-    print("\nURL:", url[0])
-    print("\nTITLE:", title[0])
-    print("\nTIMESTAMP:", title[0])
+    urls = selector.css(".tec--card__title a::attr(href)").getall()
+    # Talvez utilizar o array url para pegar os outros
+    # atributos de cada página, como title e timestamp
+    detail_notice = fetcher(urls[0])
+    selector_detail_notice = Selector(text=detail_notice)
+
+    title = selector_detail_notice.css(
+        ".tec--article__header__title::text"
+    ).get()
+
+    timestamp = selector_detail_notice.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+
+    writer = selector_detail_notice.css(
+        ".tec--author__info__link::text"
+    ).get()
+
+    # shares_count e comments_count são numéricos
+    # Talvez seja necessário fazer uma verificação do shares_count,
+    # pois nem todas as páginas tem, e colocar seu valor padrão como zero
+    shares_count = int(
+        selector_detail_notice.css(
+            ".tec--toolbar__item::text"
+        ).get()
+        [:-len("Compartilharam")]
+    )
+    comments_count = int(
+        selector_detail_notice.css(
+            ".tec--toolbar__item button::attr(data-count)"
+        ).get()
+    )
+    print("\nURL:", urls[0])
+    print("TITLE:", title)
+    print("TIMESTAMP:", timestamp)
+    print("WRITE:", writer)
+    print("SHARES_COUNT:", shares_count)
+    print("COMMENTS_COUNT:", comments_count)
 
     # news.append(new)
     # url = selector.css(".tec--list").getall()
-    print("\nSELECTOR:", selector)
+    # print("\nSELECTOR:", detail_notice)
     # print(url)
     return selector
 

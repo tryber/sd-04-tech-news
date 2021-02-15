848f282417d023face9d8c8439556e5b894e0243
Tiago-ers <trsilva284@gmail.com>
2021-02-15 18:07:20 -0300

    formatacao

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -28,7 +28,8 @@ def scrape(fetcher=Url, pages=1):
 
     selector = parsel.Selector(text=response.text)
     # obtendo todos os links de notícias (href)
-    response_all_link_news = selector.css('.tec--card__info h3 a::attr(href)').getall()
+    response_all_link_news = \
+        selector.css('.tec--card__info h3 a::attr(href)').getall()
     # print(response_all_link_news)
     # obtendo as noticias de cada link
     for link_news in response_all_link_news:
@@ -39,7 +40,8 @@ def scrape(fetcher=Url, pages=1):
         timestamp = selector.css("time::attr(datetime)").get()
         author = selector.css(".tec--author__info__link::text").get()
         shares_count = selector.css(".tec--toolbar__item *::text").get()
-        comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+        comments_count = \
+            selector.css("#js-comments-btn::attr(data-count)").get()
         summary = selector.css(".tec--article__body.z--px-16 *::text").get()
         sources = selector.css(".z--mb-16.z--px-16 a::text").getall()
         categories = selector.css("#js-categories a::text").getall()

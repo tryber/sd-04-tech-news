2bbae6eeac8f6d2bac90cea1713cc4428803e973
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-16 18:59:09 -0300

    Req.5 - search_engine - search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,16 @@
+from tech_news.database import search_news
+
+
+def make_tuple(data):
+    new_tuple = []
+    for news in data:
+        new_tuple.append((news["title"], news["url"]))
+    return new_tuple
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    result = search_news({"title": {"$regex": title, "$options": 'im'}})
+    return make_tuple(result)
 
 
 def search_by_date(date):
@@ -12,3 +23,11 @@ def search_by_source(source):
 
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+# Teste local
+# python3 -i tech_news/analyzer/search_engine.py
+# search_by_title("Musk")
+# search_by_date("2020-11-11")
+# search_by_source("Venture Beat")
+# search_by_category("Tesla")

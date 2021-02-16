4e6d75373cf9fa002f099e32b4cab7d823b9621f
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-15 23:32:35 -0300

    scrapper.py - scrape funcionando, passando em testes locais

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -19,103 +19,135 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    news = []
+    notices = []
     base_url = "https://www.tecmundo.com.br/novidades?page="
-    response = fetcher(f"{base_url}{pages}")
-    # print("\nRESPONSE:", response)
-    selector = Selector(text=response)
 
-    # Links para acesso da notícia
-    urls = selector.css(".tec--card__title a::attr(href)").getall()
+    # Percorre o número de páginas dado em 'pages'
+    for i in range(1, pages + 1):
+        response = fetcher(f"{base_url}{i}")
+        # print("\nRESPONSE:", response)
+        selector = Selector(text=response)
 
-    # Talvez utilizar o array urls para pegar os outros
-    # atributos de cada página, como title e timestamp
-    detail_notice = fetcher(urls[0])
-    selector_detail_notice = Selector(text=detail_notice)
+        # Links para acesso da notícia
+        # urls = selector.css(".tec--card__title a::attr(href)").getall()
+        urls = selector.css(".tec--card__info h3 a::attr(href)").getall()
+        print("\nLEN_URLS:", len(urls))
+        print("URLS:", urls)
+        for url in urls:
+            # Talvez utilizar o array urls para pegar os outros
+            # atributos de cada página, como title e timestamp
+            detail_notice = fetcher(url)
+            selector_detail_notice = Selector(text=detail_notice)
 
-    # Título da notícia
-    title = selector_detail_notice.css(
-        ".tec--article__header__title::text"
-    ).get()
+            # Título da notícia
+            title = selector_detail_notice.css(
+                ".tec--article__header__title::text"
+            ).get()
 
-    # Data e hora da notícia
-    timestamp = selector_detail_notice.css(
-        ".tec--timestamp__item time::attr(datetime)"
-    ).get()
+            # Data e hora da notícia
+            timestamp = selector_detail_notice.css(
+                ".tec--timestamp__item time::attr(datetime)"
+            ).get()
 
-    # Autor da notícia
-    writer = selector_detail_notice.css(".tec--author__info__link::text").get()
+            # Autor da notícia
+            writer = selector_detail_notice.css(
+                ".tec--author__info__link::text"
+            ).get()
 
-    # shares_count e comments_count são numéricos
-    # Talvez seja necessário fazer uma verificação do shares_count,
-    # pois nem todas as páginas tem, e colocar seu valor padrão como zero
-    # Número de Compartilhamentos
-    shares_count = int(
-        selector_detail_notice.css(".tec--toolbar__item::text").get()[
-            : -len("Compartilharam")
-        ]
-    )
-    # Número de Comentários
-    comments_count = int(
-        selector_detail_notice.css(
-            ".tec--toolbar__item button::attr(data-count)"
-        ).get()
-    )
-    # Primeiro parágrafo é o resumo
-    # Primeiro parágrafo
-    first_paragraph = selector_detail_notice.css(".tec--article__body p").get()
-    # Texto do primeiro parágrafo dentro de um array
-    text_nodes_first_paragraph = (
-        Selector(text=first_paragraph).css("*::text").getall()
-    )
-    # Transformando o resumo em string
-    summary = ""
-    for phrases in text_nodes_first_paragraph:
-        summary += phrases
+            # shares_count e comments_count são numéricos
+            # Talvez seja necessário fazer uma verificação do shares_count,
+            # pois nem todas as páginas tem,
+            # e colocar seu valor padrão como zero
+            # Número de Compartilhamentos
+            shares_count_str = selector_detail_notice.css(
+                ".tec--toolbar__item::text"
+            ).get()
+            if not shares_count_str:
+                shares_count = 0
+            else:
+                shares_count = int(shares_count_str[: -len("Compartilharam")])
+            # Número de Comentários
+            comments_count_str = selector_detail_notice.css(
+                ".tec--toolbar__item button::attr(data-count)"
+            ).get()
+            comments_count = int(comments_count_str)
+            # Primeiro parágrafo é o resumo
+            # Primeiro parágrafo
+            # detail_notice = fetcher(url)
+            # selector_detail_notice = Selector(text=detail_notice)
+            # print("\nSELECTOR_DETAIL_NOTICE:", selector_detail_notice)
+            # print("URL:", url)
+            first_paragraph = selector_detail_notice.css(
+                ".tec--article__body > p"
+            ).get()
+            while first_paragraph is None:
+                detail_notice = fetcher(url)
+                selector_detail_notice = Selector(text=detail_notice)
+                first_paragraph = selector_detail_notice.css(
+                    ".tec--article__body > p"
+                ).get()
+            # print("\nFIRST_PARAGRAPH:", first_paragraph)
+            # Texto do primeiro parágrafo dentro de um array
+            text_nodes_first_paragraph = (
+                Selector(text=first_paragraph).css("*::text").getall()
+            )
+            # print("TEXT_NODES_FIRST_PARAGRAPH:", text_nodes_first_paragraph)
+            # Transformando o resumo em string
+            summary = ""
+            for phrases in text_nodes_first_paragraph:
+                summary += phrases
+            # summary = selector_detail_notice.css(
+            #     "div.tec--article__body > p::text").get()
+            # print("SUMMARY:", summary)
 
-    # Fontes
-    get_sources = selector_detail_notice.css(
-        ".z--mb-16 div a.tec--badge::text"
-    ).getall()
-    sources = []
-    for source in get_sources:
-        sources.append(source.strip())
+            # Fontes
+            sources = selector_detail_notice.css(
+                ".z--mb-16 div a.tec--badge::text"
+            ).getall()
+            # sources = []
+            # # Retira espaço em branco de 'sources'
+            # for source in get_sources:
+            #     sources.append(source.strip())
 
-    # Categorias
-    get_categories = selector_detail_notice.css(
-        "#js-categories a::text"
-    ).getall()
-    categories = []
-    for categorie in get_categories:
-        categories.append(categorie.strip())
-    print("\nURL:", urls[0])
-    print("TITLE:", title)
-    print("TIMESTAMP:", timestamp)
-    print("WRITE:", writer)
-    print("SHARES_COUNT:", shares_count)
-    print("COMMENTS_COUNT:", comments_count)
-    # print("\nFIRST_PARAGRAPH:", first_paragraph)
-    # print("TEXT_NODES_FIRST_PARAGRAPH:", text_nodes_first_paragraph)
-    print("SUMMARY:", summary)
-    print("SOURCES:", sources)
-    print("CATEGORIES:", categories)
-    news.append({
-        "url": urls[0],
-        "title": title,
-        "timestamp": timestamp,
-        "writer": writer,
-        "shares_count": shares_count,
-        "comments_count": comments_count,
-        "summary": summary,
-        "sources": sources,
-        "categories": categories
-    })
-    print("\nNEWS: ", news)
-    # news.append(new)
-    # url = selector.css(".tec--list").getall()
+            # Categorias
+            categories = selector_detail_notice.css(
+                "#js-categories a::text"
+            ).getall()
+            # categories = []
+            # Retira espaço em branco de 'categories'
+            # for categorie in get_categories:
+            #     categories.append(categorie.strip())
+            # print("\nURL:", urls[0])
+            # print("TITLE:", title)
+            # print("TIMESTAMP:", timestamp)
+            # print("WRITE:", writer)
+            # print("SHARES_COUNT:", shares_count)
+            # print("COMMENTS_COUNT:", comments_count)
+            # print("\nFIRST_PARAGRAPH:", first_paragraph)
+            # print("TEXT_NODES_FIRST_PARAGRAPH:", text_nodes_first_paragraph)
+            # print("SUMMARY:", summary)
+            # print("SOURCES:", sources)
+            # print("CATEGORIES:", categories)
+            notices.append(
+                {
+                    "url": url,
+                    "title": title,
+                    "timestamp": timestamp,
+                    "writer": writer,
+                    "shares_count": shares_count,
+                    "comments_count": comments_count,
+                    "summary": summary,
+                    "sources": sources,
+                    "categories": categories,
+                }
+            )
+            # print("\nNEWS: ", news)
+
     # print("\nSELECTOR:", detail_notice)
     # print(url)
-    return selector
+    # print("\n\nNOTICES:", notices)
+    print("\nLEN_NOTICES:", len(notices))
+    return notices
 
 
 # Teste Local

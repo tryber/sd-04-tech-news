26ddbffda4be5534eef9c323979efa1c57f8048d
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-16 15:23:35 -0300

    importer.py - Problema de complexidade resolvido

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,37 +1,42 @@
-import os
 import csv
+
+
+# Funções auxiliares para fazer o csv_importer
+def make_csv_list(reader_csv, required_header):
+    list_csv = []
+    for row in reader_csv:
+        element = {}
+        for header in required_header:
+            element[header] = row[header]
+        list_csv.append(element)
+    return list_csv
 
 
 def csv_importer(filepath):
     if not filepath.endswith(".csv"):
         raise ValueError("Formato invalido")
 
-    if not os.path.exists(filepath):
+    try:
+        with open(filepath) as file_csv:
+            reader_csv = csv.DictReader(file_csv, delimiter=";", quotechar='"')
+
+            required_header = [
+                "url",
+                "title",
+                "timestamp",
+                "writer",
+                "shares_count",
+                "comments_count",
+                "summary",
+                "sources",
+                "categories",
+            ]
+
+            list_csv = make_csv_list(reader_csv, required_header)
+
+    except KeyError:
+        raise ValueError("Formato invalido")
+    except FileNotFoundError:
         raise ValueError(f"Arquivo {filepath} não encontrado")
 
-    with open(filepath) as file_csv:
-        reader_csv = csv.DictReader(file_csv, delimiter=";", quotechar='"')
-
-        required_header = [
-            "url",
-            "title",
-            "timestamp",
-            "writer",
-            "shares_count",
-            "comments_count",
-            "summary",
-            "sources",
-            "categories",
-        ]
-
-        try:
-            list_csv = []
-            for row in reader_csv:
-                element = {}
-                for header in required_header:
-                    element[header] = row[header]
-                list_csv.append(element)
-        except KeyError:
-            raise ValueError("Formato invalido")
-
-        return list_csv
+    return list_csv

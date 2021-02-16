6bfa1f253eadd7779b1f7edc7f6c27591cbdc868
Ronald Lima <ronaldazevedolima@gmail.com>
2021-02-16 18:35:40 -0300

    lint

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -28,5 +28,6 @@ def fetch_content(url, timeout=3, delay=0.5):
         # (https://www.w3schools.com/python/ref_requests_response.asp)
         return response.text
 
+
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""

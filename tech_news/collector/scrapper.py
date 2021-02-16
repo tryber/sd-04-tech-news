1a08cf51b6373faee6e2b88a64cb79a7ab9a574f
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-15 23:39:37 -0300

    scrapper.py - retirada de prints e comentarios

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -25,17 +25,11 @@ def scrape(fetcher, pages=1):
     # Percorre o número de páginas dado em 'pages'
     for i in range(1, pages + 1):
         response = fetcher(f"{base_url}{i}")
-        # print("\nRESPONSE:", response)
         selector = Selector(text=response)
 
         # Links para acesso da notícia
-        # urls = selector.css(".tec--card__title a::attr(href)").getall()
         urls = selector.css(".tec--card__info h3 a::attr(href)").getall()
-        print("\nLEN_URLS:", len(urls))
-        print("URLS:", urls)
         for url in urls:
-            # Talvez utilizar o array urls para pegar os outros
-            # atributos de cada página, como title e timestamp
             detail_notice = fetcher(url)
             selector_detail_notice = Selector(text=detail_notice)
 
@@ -55,9 +49,11 @@ def scrape(fetcher, pages=1):
             ).get()
 
             # shares_count e comments_count são numéricos
-            # Talvez seja necessário fazer uma verificação do shares_count,
-            # pois nem todas as páginas tem,
-            # e colocar seu valor padrão como zero
+            """
+            Talvez seja necessário fazer uma verificação do
+            shares_count, pois nem todas as páginas tem,
+            e colocar seu valor padrão como zero
+            """
             # Número de Compartilhamentos
             shares_count_str = selector_detail_notice.css(
                 ".tec--toolbar__item::text"
@@ -66,46 +62,42 @@ def scrape(fetcher, pages=1):
                 shares_count = 0
             else:
                 shares_count = int(shares_count_str[: -len("Compartilharam")])
+
             # Número de Comentários
             comments_count_str = selector_detail_notice.css(
                 ".tec--toolbar__item button::attr(data-count)"
             ).get()
             comments_count = int(comments_count_str)
+
             # Primeiro parágrafo é o resumo
-            # Primeiro parágrafo
-            # detail_notice = fetcher(url)
-            # selector_detail_notice = Selector(text=detail_notice)
-            # print("\nSELECTOR_DETAIL_NOTICE:", selector_detail_notice)
-            # print("URL:", url)
             first_paragraph = selector_detail_notice.css(
                 ".tec--article__body > p"
             ).get()
+            """
+            'first_paragraph' corre o risco de vir 'None',
+            caso isso ocorra fazer o requisito novamente
+            """
             while first_paragraph is None:
                 detail_notice = fetcher(url)
                 selector_detail_notice = Selector(text=detail_notice)
                 first_paragraph = selector_detail_notice.css(
                     ".tec--article__body > p"
                 ).get()
-            # print("\nFIRST_PARAGRAPH:", first_paragraph)
             # Texto do primeiro parágrafo dentro de um array
             text_nodes_first_paragraph = (
                 Selector(text=first_paragraph).css("*::text").getall()
             )
-            # print("TEXT_NODES_FIRST_PARAGRAPH:", text_nodes_first_paragraph)
             # Transformando o resumo em string
             summary = ""
             for phrases in text_nodes_first_paragraph:
                 summary += phrases
-            # summary = selector_detail_notice.css(
-            #     "div.tec--article__body > p::text").get()
-            # print("SUMMARY:", summary)
 
             # Fontes
             sources = selector_detail_notice.css(
                 ".z--mb-16 div a.tec--badge::text"
             ).getall()
+            # Para retirar espaços em branco de 'sources'
             # sources = []
-            # # Retira espaço em branco de 'sources'
             # for source in get_sources:
             #     sources.append(source.strip())
 
@@ -113,21 +105,11 @@ def scrape(fetcher, pages=1):
             categories = selector_detail_notice.css(
                 "#js-categories a::text"
             ).getall()
+            # Para retirar espaços em branco de 'categories'
             # categories = []
-            # Retira espaço em branco de 'categories'
             # for categorie in get_categories:
             #     categories.append(categorie.strip())
-            # print("\nURL:", urls[0])
-            # print("TITLE:", title)
-            # print("TIMESTAMP:", timestamp)
-            # print("WRITE:", writer)
-            # print("SHARES_COUNT:", shares_count)
-            # print("COMMENTS_COUNT:", comments_count)
-            # print("\nFIRST_PARAGRAPH:", first_paragraph)
-            # print("TEXT_NODES_FIRST_PARAGRAPH:", text_nodes_first_paragraph)
-            # print("SUMMARY:", summary)
-            # print("SOURCES:", sources)
-            # print("CATEGORIES:", categories)
+
             notices.append(
                 {
                     "url": url,
@@ -141,12 +123,7 @@ def scrape(fetcher, pages=1):
                     "categories": categories,
                 }
             )
-            # print("\nNEWS: ", news)
 
-    # print("\nSELECTOR:", detail_notice)
-    # print(url)
-    # print("\n\nNOTICES:", notices)
-    print("\nLEN_NOTICES:", len(notices))
     return notices
 
 

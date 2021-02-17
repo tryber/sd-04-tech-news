ff40081fe6870e7f0d2554e8c6dd82deb19d842e
Tiago-ers <trsilva284@gmail.com>
2021-02-17 12:48:26 -0300

    req. 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,17 @@
+import csv
+
+
+# https://docs.python.org/pt-br/3/library/csv.html
+
 def csv_importer(filepath):
     """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    try:
+        with open(filepath, encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=";")
+            for data_row in reader:
+                data = [data_row]
+                return data
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -8,49 +8,48 @@ Url = "https://www.tecmundo.com.br/novidades"
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
     try:
-        response = requests.get(url, timeout=timeout)        
+        response = requests.get(url, timeout=timeout)
         response.raise_for_status()
-        return response.text
-    except (requests.ReadTimeout, requests.HTTPError):        
+    except (requests.ReadTimeout, requests.exceptions.HTTPError):
         return ""
-    finally:
+    else:
         time.sleep(delay)
+        return response.text
 
 
 def scrape(fetcher=Url, pages=1):
     """Seu código deve vir aqui"""
-    # Faz a requisição da url pagina 1
-    response = requests.get(fetcher + "?page=" + str(pages))
-    # contando as paginas
     list_news = []
-
-    selector = parsel.Selector(text=response.text)
-    # obtendo todos os links de notícias (href)
-    response_all_link_news = \
-        selector.css('.tec--card__info h3 a::attr(href)').getall()
-    # print(response_all_link_news)
-    # obtendo as noticias de cada link
-    for link_news in response_all_link_news:
-        # print(link_news)
-        response_news = requests.get(link_news)
-        selector = parsel.Selector(text=response_news.text)
-        title = selector.css(".tec--article__header__title *::text").get()
-        timestamp = selector.css("time::attr(datetime)").get()
-        author = selector.css(".tec--author__info__link::text").get()
-        shares_count = selector.css(".tec--toolbar__item *::text").get()
-        comments_count = \
-            selector.css("#js-comments-btn::attr(data-count)").get()
-        summary = selector.css(".tec--article__body.z--px-16 *::text").get()
-        sources = selector.css(".z--mb-16.z--px-16 a::text").getall()
-        categories = selector.css("#js-categories a::text").getall()
-        list_news.append({'url': link_news,
-                          'title': title,
-                          'timestamp': timestamp,
-                          'writer': author,
-                          'shares_count': shares_count,
-                          'comments_count': comments_count,
-                          'summary': summary,
-                          'sources': list(sources),
-                          'categories': list(categories)}),
-
-    return print(list_news)
+    page_count = 1
+    # contando as paginas
+    while page_count <= pages:
+        # Faz a requisição da url pagina 1
+        response = requests.get(fetcher + "?page=" + str({page_count}))
+        selector = parsel.Selector(text=response.text)
+        # obtendo todos os links de notícias (href)
+        response_all_link_news = \
+            selector.css('.tec--card__info h3 a::attr(href)').getall()
+        # obtendo as noticias de cada link
+        for link_news in response_all_link_news:
+            response_news = requests.get(link_news)
+            selector = parsel.Selector(text=response_news.text)
+            title = selector.css(".tec--article__header__title *::text").get()
+            timestamp = selector.css("time::attr(datetime)").get()
+            author = selector.css(".tec--author__info__link::text").get()
+            shares_count = selector.css(".tec--toolbar__item *::text").get()
+            comments_count = \
+                selector.css("#js-comments-btn::attr(data-count)").get()
+            summary = selector.css(".tec--article__body.z--px-16 *::text").get()
+            sources = selector.css(".z--mb-16.z--px-16 a::text").getall()
+            categories = selector.css("#js-categories a::text").getall()
+            list_news.append({'url': link_news,
+                              'title': title,
+                              'timestamp': timestamp,
+                              'writer': author,
+                              'shares_count': shares_count,
+                              'comments_count': comments_count,
+                              'summary': summary,
+                              'sources': list(sources),
+                              'categories': list(categories)}),
+        page_count += 1
+    return list_news

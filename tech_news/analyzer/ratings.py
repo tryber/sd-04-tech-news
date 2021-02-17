a2b3eb01a8922a3b20c4aeddcdec0e33f67f64c5
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-16 23:36:42 -0300

    ratings.py - formatado

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -9,31 +9,42 @@ def make_tuple(data):
 
 
 def top_5_news():
-    result = list(db.news.aggregate([
-        {"$addFields": {"sum_shares_comments": {"$add": ["$shares_count", "$comments_count"]}}},
-        {"$sort": {"sum_shares_comments": -1, "title": 1}},
-        {"$limit": 5}
-        ])
+    result = list(
+        db.news.aggregate(
+            [
+                {
+                    "$addFields": {
+                        "sum_shares_comments": {
+                            "$add": ["$shares_count", "$comments_count"]
+                        }
+                    }
+                },
+                {"$sort": {"sum_shares_comments": -1, "title": 1}},
+                {"$limit": 5},
+            ]
+        )
     )
-    print("\nRESULT:", result)
     return make_tuple(result)
 
 
 def top_5_categories():
-    result = list(db.news.aggregate([
-        {"$unwind": "$categories"},
-        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1, "_id": 1}},
-        {"$limit": 5}
-        ])
+    result = list(
+        db.news.aggregate(
+            [
+                {"$unwind": "$categories"},
+                {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+                {"$sort": {"count": -1, "_id": 1}},
+                {"$limit": 5},
+            ]
+        )
     )
 
     new_list = []
     for categorie in result:
         new_list.append(categorie["_id"])
-    new_list
 
     return new_list
+
 
 # Teste local
 # python3 -i tech_news/analyzer/ratings.py

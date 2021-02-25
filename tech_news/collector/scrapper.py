be43202b7d684a15f4fb9b63f5827967b6dc90c1
thiagooo14 <thiagooo15@gmail.com>
2021-02-24 21:17:17 -0300

    req 1 vqv

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,21 @@
+import requests
+from time import sleep
+
+# from parsel import Selector
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        sleep(delay)
+    except requests.ReadTimeout:
+        response = ""
+        return response
+    if response.status_code != 200:
+        response = ""
+        return response
+
+    return response.text
 
 
 def scrape(fetcher, pages=1):

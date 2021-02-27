77f570983f4a223bece723f418ad1fbca150614f
thiagooo14 <thiagooo15@gmail.com>
2021-02-27 17:56:25 -0300

    all wrong

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -13,7 +13,6 @@ def fetch_content(url, timeout=3, delay=0.5):
     if response.status_code != 200:
         response = ""
         return response
-
     return response.text
 
 
@@ -29,25 +28,27 @@ def scrape(fetcher, pages=1):
             ".tec--card__info h3 a::attr(href)"
         ).getall()
         for url in response_news:
-            selector = Selector(text=fetcher(url))
-            title = selector.css(".tec--article__header__title *::text").get()
-            timestamp = selector.css("time::attr(datetime)").get()
-            author = selector.css(".tec--author__info__link::text").get()
-            shares_count = selector.css(".tec--toolbar__item *::text").get()
-            comments_count = selector.css(
-                "#js-comments-btn::attr(data-count)"
-            ).get()
-            summary = selector.css(
-                ".tec--article__body.z--px-16 *::text"
-            ).get()
-            sources = selector.css(".z--mb-16 a::text").getall()
+            selector = Selector(fetcher(url))
+            title = selector.css("#js-article-title::text").get()
+            timestamp = selector.css("#js-article-date::attr(datetime)").get()
+            writer = selector.css(".tec--author__info__link::text").get()
+            shares_count = int(selector.css(".tec--toolbar__item::text").get())
+            comments_count = int(
+                selector.css("#js-comments-btn::attr(data-count)").get()
+            )
+            summary = "".join(
+                selector.css(
+                    ".tec--article__body p:first-of-type ::text"
+                ).getall()
+            )
+            sources = selector.css("div.z--mb-16 a::text").getall()
             categories = selector.css("#js-categories a::text").getall()
             list_news.append(
                 {
                     "url": url,
                     "title": title,
                     "timestamp": timestamp,
-                    "author": author,
+                    "writer": writer,
                     "shares_count": shares_count,
                     "comments_count": comments_count,
                     "summary": summary,

b3c02fba45a4921078d537dac7e06f2639642cfd
thiagooo14 <thiagooo15@gmail.com>
2021-02-28 15:53:34 -0300

    req 10 +lint

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -20,5 +20,19 @@ def top_5_news():
         result.append((new["title"], new["url"]))
     return result
 
+
 def top_5_categories():
-    """Seu código deve vr aqui"""
+    categories = []
+    db_result = aggregate(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+            {"$sort": {"_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+
+    for cat in db_result:
+        categories.append((cat["_id"]))
+
+    return categories

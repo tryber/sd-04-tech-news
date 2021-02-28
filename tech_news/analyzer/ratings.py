c3f7ef7866d9a144c9eda69aec2cba48d6e1e879
thiagooo14 <thiagooo15@gmail.com>
2021-02-28 15:05:30 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,24 @@
+from tech_news.database import aggregate
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = []
+    db_result = aggregate(
+        [
+            {
+                "$addFields": {
+                    "popularity": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {"$sort": {"popularity": -1, "title": 1}},
+            {"$limit": 5},
+        ]
+    )
+    result.append((db_result[0]["title"], db_result[0]["url"]))
+
+    return result
 
 
 def top_5_categories():

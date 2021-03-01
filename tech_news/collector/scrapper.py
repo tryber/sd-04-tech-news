e84122c5c136c5a8879447c9e769958b8a1b722c
leonardoinatel <leonardodamasceno@gec.inatel.br>
2021-03-01 18:23:47 -0300

    fetch_content implements

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,8 +1,20 @@
-# import request
+import requests
+from time import sleep
+
 
 def fetch_content(url, timeout=3, delay=0.5):
     print("Fetching", url)
+    try:
+        response = requests.get(url, timeout=timeout)
+        print(response.status_code)
+        sleep(delay)
+    except response.HTTPError:
+        return ""
+    if (response.status_code != 200):
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    # url = "https://www.tecmundo.com.br/novidades"

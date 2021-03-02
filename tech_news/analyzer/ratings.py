d305083fca61cd308bea6ea5f5e46cca89dafc4e
ribaslucas <lucas@mundoribas.com.br>
2021-03-02 15:16:58 -0300

    Bora que tá rolando...

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,24 @@
+from tech_news.database import aggregate_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = []
+    search = aggregate_news(
+        [
+            {
+                "$addFields": {
+                    "popularity": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {"$sort": {"popularity": -1, "title": 1}},
+            {"$limit": 5},
+        ]
+    )
+    for new in search:
+        result.append((new["title"], new["url"]))
+    return result
 
 
 def top_5_categories():

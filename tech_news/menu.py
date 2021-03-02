ebffc518aeefe001e6bcad1b1ce7351a80c27d0a
thiagooo14 <thiagooo15@gmail.com>
2021-03-02 20:18:01 -0300

    req 11

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -12,28 +12,29 @@ from tech_news.analyzer.ratings import top_5_news, top_5_categories
 import sys
 
 
-def collector_menu(input):
-    options = {
+def collector_menu():
+    options = input(
         "Selecione uma das opções a seguir:\n"
         " 1 - Importar notícias a partir de um arquivo CSV;\n"
         " 2 - Exportar notícias para CSV;\n"
         " 3 - Raspar notícias online;\n"
         " 4 - Sair.\n"
-    }
+    )
 
     if options == "1":
         file_path = input("Digite o nome do arquivo CSV a ser importado:")
         new_file = csv_importer(file_path)
         create_news(new_file)
     elif options == "2":
-        file_path = input("Digite o nome do Arquivo CSV a ser Exportado:")
-        csv_importer(file_path)
-    elif options == 3:
+        file_path = input("Digite o nome do arquivo CSV a ser exportado:")
+        csv_exporter(file_path)
+    elif options == "3":
         pages = input("Digite a quantidade de páginas a serem raspadas:")
         new_file = scrape(fetcher=fetch_content, pages=int(pages))
         create_news(new_file)
     elif options == "4":
         print("Encerrando script")
+        return
     else:
         print("Opção inválida", file=sys.stderr)
         return

d634861690531bf35949e440aa4542e53749775d
ribaslucas <lucas@mundoribas.com.br>
2021-03-02 18:20:38 -0300

    Ajustando o exporter...

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,34 +1,39 @@
-
-from tech_news.database import find_news
 import csv
+from tech_news.database import find_news
+
+exact_format = [
+    "url",
+    "title",
+    "timestamp",
+    "writer",
+    "shares_count",
+    "comments_count",
+    "summary",
+    "sources",
+    "categories",
+]
 
 
 def csv_exporter(filepath):
-
     if not filepath.endswith(".csv"):
         raise ValueError("Formato invalido")
 
-    with open(filepath, "w", newline="", encoding="utf-8") as file:
+    with open(filepath, "w") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow(exact_format)
         news = find_news()
 
-        header = [
-            "url",
-            "title",
-            "timestamp",
-            "writer",
-            "shares_count",
-            "comments_count",
-            "summary",
-            "sources",
-            "categories",
-        ]
-
-        writer = csv.writer(file, delimiter=";")
-        writer.writerow(header)
-
-        for item in news:
-            item["sources"] = ",".join(item["sources"])
-            item["categories"] = ",".join(item["categories"])
-            writer.writerow(list(item.values()))
-
-    csv_exporter("export_correct.csv")
+        for newNews in news:
+            writer.writerow(
+                [
+                    newNews["url"],
+                    newNews["title"],
+                    newNews["timestamp"],
+                    newNews["writer"],
+                    newNews["shares_count"],
+                    newNews["comments_count"],
+                    newNews["summary"],
+                    ','.join(newNews["sources"]),
+                    ','.join(newNews["categories"]),
+                ]
+            )

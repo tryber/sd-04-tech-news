2f2f85f0e204c328828c8bb6ccd9331f5be2bfc4
lourenco <loulage@gmail.com>
2021-03-03 16:28:35 -0300

    req1: implemented fetch_content function

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,14 @@
+import requests
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):

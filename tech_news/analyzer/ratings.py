19ff0001b1797060213a4237671121869d379571
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-03-03 19:45:11 -0300

    Req 9

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,28 @@
+from tech_news.database import db
+import pymongo
+
+
+# https://stackoverflow.com/questions/8109122/how-to-sort-mongodb-with-pymongo
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    lista = list(
+        db.news.find()
+        .sort(
+            [
+                ("shares_count", pymongo.ASCENDING),
+                ("comments_count", pymongo.DESCENDING),
+                "title",
+                pymongo.ASCENDING,
+            ]
+        )
+        .limit(5)
+    )
+
+    data = []
+    for x in lista:
+        data.append(x["title"], x["url"])
+    return data
 
 
 def top_5_categories():

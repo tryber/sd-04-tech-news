f0ef38008998e688f76b468d83de1c2d00a8aca1
lourenco <loulage@gmail.com>
2021-03-04 15:59:31 -0300

    imported and implemented  sleep from time to delay fetchcontent

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,9 +1,13 @@
 import requests
+from time import sleep
+from parsel import Selector
 
 
 def fetch_content(url, timeout=3, delay=0.5):
     try:
+        print("fetching content", url)
         response = requests.get(url, timeout=timeout)
+        sleep(delay)
         response.raise_for_status()
     except (requests.HTTPError, requests.ReadTimeout):
         return ""

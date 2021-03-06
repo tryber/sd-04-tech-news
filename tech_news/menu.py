5c39fa1f64fba5649a122bd96af951c90f49e210
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-03-06 16:26:43 -0300

    ultimos requisitos

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -2,13 +2,13 @@ from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.scrapper import scrape, fetch_content
 from tech_news.database import create_news
-""" from tech_news.analyzer.search_engine import (
+from tech_news.analyzer.search_engine import (
     search_by_title,
     search_by_date,
     search_by_source,
     search_by_category,
 )
-from tech_news.analyzer.ratings import top_5_news, top_5_categories """
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
 import sys
 
 
@@ -18,6 +18,24 @@ num_options = """Escolha uma das opções abaixo:
 2: Exportar notícias para CSV
 3: Raspar notícias online"
 4: Sair"""
+
+selected_option = {
+    1: "Digite o nome do arquivo CSV a ser importado:",
+    2: "Digite o nome do arquivo CSV a ser exportado:",
+    3: "Digite a quantidade de páginas a serem raspadas:",
+    4: "Encerrando Script",
+    5: "Opção inválida",
+}
+
+analyze_option = """Selecione uma das opções a seguir:
+
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por fonte;
+ 4 - Buscar notícias por categoria;
+ 5 - Listar top 5 notícias;
+ 6 - Listar top 5 categorias;
+ 7 - Sair."""
 
 
 def first_menu(num):
@@ -35,15 +53,10 @@ def open_sec_menu(select, selected):
     if select > 3:
         return
 
+# função principal do menu
+
 
 def collector_menu():
-    selected_option = {
-        1: "Digite o nome do arquivo CSV a ser importado:",
-        2: "Digite o nome do arquivo CSV a ser exportado:",
-        3: "Digite a quantidade de páginas a serem raspadas:",
-        4: "Encerrando Script",
-        5: "Opção inválida",
-    }
 
     first_menu(num_options)  # primeiro menu, 4 opções
 
@@ -65,5 +78,48 @@ def collector_menu():
         create_news(result)
 
 
+def sec_ana_option(num):
+    last_options = {
+        1: "Digite o título:",
+        2: "Digite a data no formato aaaa-mm-dd:",
+        3: "Digite a fonte:",
+        4: "Digite a categoria:",
+        7: "Encerrando script",
+        8: "Opção inválida",
+    }
+
+    if num > 7 or num < 1:
+        print(last_options[8], file=sys.stderr)
+        return
+
+    print(last_options[num])
+
+
 def analyzer_menu():
     """Seu código deve vir aqui"""
+    print(analyze_option)
+
+    try:
+        ana_input = int(input())
+    except ValueError:
+        ana_input = 8
+
+    ana_functions = {
+        1: search_by_title,
+        2: search_by_date,
+        3: search_by_source,
+        4: search_by_category,
+        5: top_5_news,
+        6: top_5_categories,
+    }
+
+    if ana_input < 5 or ana_input > 6:
+        sec_ana_option(ana_input)
+        if ana_input > 6 or ana_input < 1:
+            return
+        param_ana_input = input()
+        result = ana_functions[ana_input](param_ana_input)
+    else:
+        result = ana_functions[ana_input]()
+
+    print(result)

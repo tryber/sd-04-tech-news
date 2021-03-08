631af4321815b1f3816d45152b2898f32311cde0
leonardoinatel <leonardodamasceno@gec.inatel.br>
2021-03-08 15:26:13 -0300

    requisito 05 done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+from tech_news.database import search_news
+import re
 
+
+def search_by_title(title):
+    result = []
+    search_result = search_news(
+        {"title": {"$regex": re.compile(title, re.IGNORECASE)}}
+    )
+    
+    for news in search_result:
+        result.append((news["title"], news["url"]))
+    return result
 
 def search_by_date(date):
     """Seu código deve vir aqui"""

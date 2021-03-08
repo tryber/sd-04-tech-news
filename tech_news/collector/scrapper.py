a04b032fe01d93038a9c8f7c79c1a84e4edb56f0
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-03-08 15:29:28 -0300

    Passando pelo linter

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -2,6 +2,7 @@ import requests
 import time
 from time import sleep
 
+
 def fetch_content(url, timeout=3, delay=0.5):
      time.sleep(delay)
     try:
@@ -12,7 +13,6 @@ def fetch_content(url, timeout=3, delay=0.5):
         return ""
     else:
         return resp.text
-    
     '''
     resp = requests.get(url, timeout=timeout)
     if resp.status_code != 200:
@@ -20,5 +20,6 @@ def fetch_content(url, timeout=3, delay=0.5):
     time.sleep(delay)
     return resp.text
     '''
+
 
 def scrape(fetcher, pages=1):

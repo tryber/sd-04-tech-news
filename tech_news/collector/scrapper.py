f23f2115cecbec33dd3b015a90c463ba2fd67d59
djonata-dias <diasdjonata@gmail.com>
2021-03-08 21:15:12 -0300

    função fetch_content e scrape prontas para raspagem de dados

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,58 @@
+import requests
+from time import sleep
+from parsel import Selector
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    print(requests.codes.ok)
+    try:
+        response = requests.get(url, timeout=timeout)
+        sleep(delay)
+    except requests.ReadTimeout:
+        response = ""
+        return response
+    if response.status_code != requests.codes.ok:
+        response = ""
+        return response
+    return response.text
+
+
+URL = 'https://www.tecmundo.com.br/novidades'
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    articlesList = []
+    for page in range(1, pages + 1):
+        responseText = fetcher(f"{URL}?page={page}")
+        selector = Selector(text=responseText)
+        selectorLinks = selector.css(
+            '.tec--list__item .tec--card__title__link::attr(href)').getall()
+        for link in selectorLinks:
+            articleDic = {}
+            articleDic["url"] = link
+            responseText1 = fetcher(link)
+            selector = Selector(text=responseText1)
+            articleDic["title"] = selector.css('#js-article-title::text').get()
+            articleDic["timestamp"] = selector.css(
+                '#js-article-date::attr(datetime)').get()
+            articleDic["writer"] = selector.css('.z--font-bold a::text').get()
+            articleDic["shares_count"] = selector.css(
+                '#js-author-bar .tec--toolbar__item::text').get()
+            if articleDic["shares_count"]:
+                articleDic["shares_count"] = int(
+                    articleDic["shares_count"].split()[0])
+            articleDic["comments_count"] = int(selector.css(
+                '#js-comments-btn::attr(data-count)').get())
+            articleDic["summary"] = selector.css(
+                '.tec--article__body p:first-of-type::text').get()
+            articleDic["sources"] = selector.css(
+                '.z--mb-16 .tec--badge::text').getall()
+            articleDic["categories"] = selector.css(
+                '#js-categories a::text').getall()
+            articlesList.append(articleDic)
+    return articlesList
+
+# scrape(fetch_content)
+
+
+# summary

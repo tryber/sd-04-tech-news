f1d9a54de4869d09a88980bab518662166273dce
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-03-10 20:59:45 -0300

    requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,9 @@
+import datetime
 from tech_news.database import search_news
 
 
 def search_by_title(title):
+    """Busca noticia pelo titulo se nao existir retorna uma list vazia"""
     new_list = []
     info = search_news({"title": {"$regex": title, "$options": "i"}})
     for new in info:
@@ -10,7 +12,17 @@ def search_by_title(title):
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    """Busca noticia pela data se nao existir retorna um list vazia"""
+    try:
+        datetime.datetime.strftime(date, "%y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        new_list = []
+        data = search_news({"timestamp": {"$regex": date}})
+        for new in data:
+            new_list.append((new["title"], new["url"]))
+            return new_list
 
 
 def search_by_source(source):

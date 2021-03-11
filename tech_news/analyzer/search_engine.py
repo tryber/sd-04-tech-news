c5b6a8a392cbd1d42169b54731bb5fe39af13a90
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-03-10 21:13:34 -0300

    teste req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -14,15 +14,16 @@ def search_by_title(title):
 def search_by_date(date):
     """Busca noticia pela data se nao existir retorna um list vazia"""
     try:
-        datetime.datetime.strftime(date, "%Y-%m-%d")
+        datetime.strptime(date, "%Y-%m-%d")
+        data = search_news({"timestamp": {"$regex": date}})
+        if data == []:
+            return []
+        new_list = []
+        for item in data:
+            new_list.append((item["title"], item["url"]))
+        return new_list
     except ValueError:
         raise ValueError("Data inválida")
-    else:
-        new_list = []
-        data = search_news({"timestamp": {"$regex": date}})
-        for new in data:
-            new_list.append((new["title"], new["url"]))
-            return new_list
 
 
 def search_by_source(source):

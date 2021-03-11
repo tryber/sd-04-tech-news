b9fc9e6d8664242b1e36e7849a05e535ef288962
djonata-dias <diasdjonata@gmail.com>
2021-03-11 01:02:15 -0300

    ajuste do menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,14 +1,13 @@
 import sys
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.importer import csv_importer
-from tech_news.collector.scrapper import (fetch_content, scrape)
-from tech_news.analyzer.ratings import (top_5_categories, top_5_news)
+from tech_news.collector.scrapper import fetch_content, scrape
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
 from tech_news.analyzer.search_engine import (
     search_by_category,
     search_by_date,
     search_by_source,
     search_by_title,
-    search_news
 )
 from tech_news.database import create_news
 

73d6b83f2abd62ac6fb6934fa69e6f7215be7ba4
Diego <diegodiko05@gmail.com>
2021-03-10 21:39:48 -0300

    Requisito 4

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,38 @@
+import csv
+from tech_news.database import find_news
+
+FIELDS = [
+    "url",
+    "title",
+    "timestamp",
+    "writer",
+    "shares_count",
+    "comments_count",
+    "summary",
+    "sources",
+    "categories",
+]
+
+
+def join_list():
+    content = find_news()
+    for field in content:
+        for key in field:
+            if isinstance(field[key], list):
+                field[key] = ",".join(field[key])
+    return content
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    try:
+        file = open(filepath, "w")
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
+    else:
+        writed_file = csv.DictWriter(
+            file, fieldnames=FIELDS, delimiter=";"
+        )
+        writed_file.writeheader()
+        writed_file.writerows(join_list())

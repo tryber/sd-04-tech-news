e5a0cb22e8ade3171ed9c6e46242e48e2e0f6554
djonata-dias <diasdjonata@gmail.com>
2021-03-10 22:55:11 -0300

    ajuste na importação do database

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,8 @@
 import datetime
 from pymongo import MongoClient
+
 client = MongoClient()
-from tech_news.database import search_news 
+from ..database import search_news
 
 
 def search_by_title(title):
@@ -44,4 +45,3 @@ def search_by_category(category):
         results.append((new["title"], new["url"]))
 
     return results
-

162d44d640277a07ba6542640eeba1198afca417
richardwellerson <richardwellerson@gmail.com>
2021-03-11 20:19:29 -0300

    TechNews: 1 Req

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,11 @@
+import requests
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    data = requests.get(url)
+    if (data.status_code != 200):
+        return ''
+    return data.content
 
 
 def scrape(fetcher, pages=1):

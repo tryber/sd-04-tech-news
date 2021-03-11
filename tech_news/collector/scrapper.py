d41f057bc6bbac28e5d7206ec56c3574167e8769
Diego <diegodiko05@gmail.com>
2021-03-10 21:05:00 -0300

    Diego Campos - Tech News

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,17 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
+        return ''
+    else:
+        return response.text
+    finally:
+        time.sleep(delay)
 
 
 def scrape(fetcher, pages=1):

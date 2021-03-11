f5035196df2765a4b8d2a4ef8b3a04fa10a55fbe
Diego <diegodiko05@gmail.com>
2021-03-10 21:24:43 -0300

    req 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,15 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    try:
+        csv_file = open(filepath)
+    except FileNotFoundError:
+        return  ValueError(f"Arquivo {filepath} não encontrado")
+    else:
+        read_content = csv.DictReader(csv_file, delimiter=";")
+        for content in read_content:
+            values = content
+            return [values]

7badf36067629a2b063618619775eecbdfb45a96
gabrielukke <gabriellukk@outlook.com>
2021-03-12 19:06:44 -0300

    Req 2 - All Tests

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+from parsel import Selector
 
 
 def fetch_content(url, timeout=3, delay=0.5):
@@ -16,7 +17,57 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    URL = "https://www.tecmundo.com.br/novidades?page="
+    news = []
 
+    for page in range(1, pages + 1):
+        url_selector = Selector(fetcher(f"{URL}{page}"))
+        news_urls_list = url_selector.css(
+            ".tec--list__item .tec--card__title__link::attr(href)"
+        ).getall()
 
-print(fetch_content("https://www.tecmundo.com.br/404"))
+        for news_url in news_urls_list:
+            selector = Selector(fetcher(news_url))
+
+            shares_count = selector.css(".tec--toolbar__item::text").get()
+            if shares_count is None:
+                shares_count = 0
+            else:
+                shares_count = int(
+                    (
+                        selector.css(".tec--toolbar__item::text")
+                        .get()
+                        .replace(" Compartilharam", "")
+                    )
+                )
+
+            news.append(
+                {
+                    "url": news_url,
+                    "title": selector.css(
+                        ".tec--article__header__title::text"
+                    ).get(),
+                    "timestamp": selector.css(
+                        ".tec--timestamp__item time::attr(datetime)"
+                    ).get(),
+                    "writer": selector.css(
+                        ".tec--author__info__link::text"
+                    ).get(),
+                    "shares_count": shares_count,
+                    "comments_count": int(
+                        selector.css(
+                            ".tec--toolbar__item button::attr(data-count)"
+                        ).get()
+                    ),
+                    "summary": selector.css(
+                        ".tec--article__body *::text"
+                    ).get(),
+                    "sources": selector.css(
+                        "div.z--mb-16 .tec--badge::text"
+                    ).getall(),
+                    "categories": selector.css(
+                        "#js-categories .tec--badge::text"
+                    ).getall(),
+                }
+            )
+    return news

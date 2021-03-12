87f584329986c99e2206e0e41e7a09a1f7365f64
lourenco <loulage@gmail.com>
2021-03-12 16:57:58 -0300

    refactor scrapper.py due to int error on shares_count/comments_count on pages n>1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -15,53 +15,43 @@ def fetch_content(url, timeout=3, delay=0.5):
         return response.text
 
 
+URL_BASE = "https://www.tecmundo.com.br/novidades"
+
+
+def scrape_news(url, selector):
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    writer = selector.css("a.tec--author__info__link::text").get()
+    shares_count = (
+        selector.css(".tec--toolbar__item::text").re_first(r"\d") or "0"
+    )
+    comments_count = (
+        selector.css("#js-comments-btn::text").re_first(r"\d") or "0"
+    )
+    summary = selector.css(".tec--article__body > p::text").get()
+    sources = selector.css("div.z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("#js-categories a::text").getall()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+
+
 def scrape(fetcher, pages=1):
-    base_url = "https://www.tecmundo.com.br/novidades?page="
-    page_number = 1
-
-    all_news = []
-
-    while page_number <= pages:
-        content = fetcher(f"{base_url}{page_number}")
-        selector = Selector(content)
-        news_container = selector.css("div.tec--list__item")
-        for item in news_container:
-            url = selector.css("h3 a::att(href)").get()
-            details_content = fetcher(url)
-            details_selector = Selector(details_content)
-            title = details_selector.css("#js-article-title::text").get()
-            timestamp = details_selector.css(
-                ".tec--timestamp__item time::attr(datetime)"
-            ).get()
-            writer = details_selector.css(
-                ".tec--author__info__link::text"
-            ).get()
-            shares = details_selector.css("button::attr(data-count)").get()
-            comments = details_selector.css(
-                ".tec--btn::attr(data-count)"
-            ).getall()
-            summary = details_selector.css(
-                ".tec--article__body > p::text"
-            ).get()
-            sources = details_selector.css(".z--mb-16 > div a::text").getall()
-            categories = details_selector.css(
-                "#js-categories a::text"
-            ).getall()
-
-            news = {
-                "url": url,
-                "title": title,
-                "timestamp": timestamp,
-                "writer": writer,
-                "shares_count": int(shares),
-                "comments_count": int(comments[0]),
-                "summary": summary,
-                "sources": sources,
-                "categories": categories,
-            }
-
-            all_news.append(news)
-
-        page_number += 1
-
-    return all_news
+    news_object = []
+    for page in range(1, pages + 1):
+        selector = Selector(fetcher(f"{URL_BASE}?page={page}"))
+        get_urls = selector.css(
+            ".tec--list__item .tec--card__title__link::attr(href)"
+        ).getall()
+        for url in get_urls:
+            selector = Selector(fetcher(url))
+            news_object.append(scrape_news(url, selector))
+    return news_object

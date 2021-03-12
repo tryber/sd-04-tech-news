302f02cd16b8f92a1abbe78790686e1bd93abeaa
lourenco <loulage@gmail.com>
2021-03-12 15:11:42 -0300

    implemented top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,23 @@
+from tech_news.analyzer.search_engine import convert_to_tuples
+from tech_news.database import search_news_with_agregations
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = search_news_with_agregations(
+        [
+            {
+                "$addFields": {
+                    "popularity": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {"$sort": {"popularity": -1, "title": 1}},
+            {"$limit": 5},
+        ]
+    )
+    # print(news)
+    return convert_to_tuples(news)
 
 
 def top_5_categories():

30d380d6265293e0d8c0332ca7116bf152b4b46a
matheusysd <matheusyurisd@gmail.com>
2021-03-13 20:38:24 -0300

    Initial commit

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,16 @@
-def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+import requests
+import time
 
+def fetch_content(url, timeout=3, delay=0.5):
+ try:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+    except requests.ReadTimeout:
+        return ""
+    if response.status_code == 200:
+        return response.text
+    else:
+        return ""
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""

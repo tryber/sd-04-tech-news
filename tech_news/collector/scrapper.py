4f6c6470729eb4ff5e96978b11ee73005ab403e8
lumaarrud <luma.oarruda@gmail.com>
2021-03-13 14:30:33 -0300

    req 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,19 @@
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    print("Fetching", url)
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+        sleep(delay)
+    except (requests.ReadTimeout, requests.exceptions.HTTPError):
+        return ""
+    if (response.status_code != 200):
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):

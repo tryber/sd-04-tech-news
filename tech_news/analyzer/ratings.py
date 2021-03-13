a67c6b64da61608c090f7769d0e4acdca1e808c8
lumaarrud <luma.oarruda@gmail.com>
2021-03-13 15:23:26 -0300

    req 9 - 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,40 @@
+from tech_news.database import search_news_agregation
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    list_result = search_news_agregation(
+        [
+            {
+                "$addFields": {
+                    "popularity": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {"$sort": {"popularity": -1, "title": 1}},
+            {"$limit": 5},
+        ]
+    )
+    list_top5 = []
+    if list_result == []:
+        return []
+    for new in list_result:
+        list_top5.append((new["title"], new["url"]))
+    return list_top5
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    list_result = search_news_agregation(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1, "_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+    list_top5_categories = []
+    if list_result == []:
+        return []
+    for new in list_result:
+        list_top5_categories.append((new["_id"]))
+    return list_top5_categories

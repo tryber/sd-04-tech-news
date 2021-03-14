bf92c0071e0b6e10c6f17bb7aa45e23d3fb079cc
matheusysd <matheusyurisd@gmail.com>
2021-03-13 21:42:05 -0300

    Search by category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -35,4 +35,11 @@ def search_by_source(source):
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = database.search_news(
+        {"categories": {"$regex": category, "$options": "-i"}}
+    )
+
+    if len(news) == 0:
+        return news
+
+    return [(news[0]["title"], news[0]["url"])]

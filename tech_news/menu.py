6494542b4d0f5576cfc19f0396a217062b3754d9
pedrotpo <pedrotpoliveira@gmail.com>
2021-03-14 10:51:46 -0300

    Menu Analyzer

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -4,12 +4,12 @@ from tech_news.collector.scrapper import scrape, fetch_content
 from tech_news.database import create_news
 
 # from tech_news.analyzer.ratings import top_5_news, top_5_categories
-# from tech_news.analyzer.search_engine import (
-#     search_by_title,
-#     search_by_date,
-#     search_by_source,
-#     search_by_category,
-# )
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_source,
+    #     search_by_category,
+)
 import sys
 
 
@@ -54,4 +54,26 @@ def collector_menu():
 
 
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    choice = input(
+        "Selecione uma das opções a seguir:\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por fonte;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair."
+    )
+    if choice == "1":
+        title = input("Digite o título:")
+        search_by_title(title)
+    elif choice == "2":
+        date = input("Digite a data no formato aaaa-mm-dd::")
+        search_by_date(date)
+    elif choice == "3":
+        source = input("Digite a fonte:")
+        search_by_source(source)
+    elif choice == "7":
+        return print("Encerrando script")
+    else:
+        return print("Opção inválida", file=sys.stderr)

9c987d22d878f28163a930bf5a63e1d49bbf5e98
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-03-14 16:03:12 -0300

    Creat top_5_categories function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,29 +1,30 @@
-from tech_news.database import search_news_agregate
-
-
-def create_lists(data):
-    if data == []:
-        return []
-    search_for_news = []
-    for new in data:
-        search_for_news.append((new["title"], new["url"]))
-    return search_for_news
+from tech_news.database import db
+from bson.son import SON
+import pymongo
 
 
 def top_5_news():
-    news = search_news_agregate(
-        [
-            {
-                "$addFields": {
-                    "popularity": {"$add:"["$shares_count", "$comments_count"]}
-                }
-            },
-            {"$sort": {"popularity": -1, "title": 1}},
-            {"$limit": 5},
-        ]
+    top_5_news_list = (
+        db.news.find({})
+        .sort(
+            [
+                ("shares_count", pymongo.DESCENDING),
+                ("comments_count", pymongo.DESCENDING),
+                ("title", pymongo.ASCENDING),
+            ]
+        )
+        .limit(5)
     )
-    return create_lists(news)
+    return [(new["title"], new["url"]) for new in top_5_news_list]
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    top_5_categories_list = db.news.aggregate(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+            {"$sort": SON([("count", 1), ("_id", 1)])},
+            {"$limit": 5},
+        ]
+    )
+    return [(new["_id"]) for new in top_5_categories_list]

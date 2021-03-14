ecb95d48e4dcfca94972a80596c357051cbb70b9
richardwellerson <richardwellerson@gmail.com>
2021-03-14 08:44:57 -0300

    Technews: Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -19,4 +19,14 @@ def top_5_news():
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    data = db.news.aggregate([
+        {"$unwind": "$categories"},
+        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+        {"$sort": SON([("count", 1), ("_id", 1)])},
+        {"$limit": 5},
+    ])
+    news_data = []
+    for i in data:
+        news_data.append((i["_id"]))
+
+    return news_data

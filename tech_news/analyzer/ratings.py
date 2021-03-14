8a2e99474424f2896aa18c7525bc22251366f83b
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-03-14 15:43:43 -0300

    Create top_5_news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,28 @@
+from tech_news.database import search_news_agregate
+
+
+def create_lists(data):
+    if data == []:
+        return []
+    search_for_news = []
+    for new in data:
+        search_for_news.append((new["title"], new["url"]))
+    return search_for_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = search_news_agregate(
+        [
+            {
+                "$addFields": {
+                    "popularity": {"$add:"["$shares_count", "$comments_count"]}
+                }
+            },
+            {"$sort": {"popularity": -1, "title": 1}},
+            {"$limit": 5},
+        ]
+    )
+    return create_lists(news)
 
 
 def top_5_categories():

f243f9bc833397f22dcc30b29ea6ccd6fa4430b4
fogo5000 <lucaslucas2008@gmail.com>
2021-03-14 04:05:11 -0300

    Requisition 4 done

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,29 @@
+import csv
+from tech_news.database import find_news
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    data = find_news()
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    with open(filepath, "w") as file:
+        file_writer = csv.DictWriter(file, delimiter=";", fieldnames=[
+            "url",
+            "title",
+            "timestamp",
+            "writer",
+            "shares_count",
+            "comments_count",
+            "summary",
+            "sources",
+            "categories",
+        ])
+
+        for item in data:
+            for indice in item:
+                if isinstance(item[indice], list):
+                    item[indice] = ",".join(item[indice])
+
+        file_writer.writeheader()
+        file_writer.writerows(data)
+        file.close()

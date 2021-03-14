3a619f42c18f7837eec85b7af58ba6551fd6547a
gabrielukke <gabriellukk@outlook.com>
2021-03-14 07:25:49 -0300

    Req 9 - all tests

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,24 @@
+from tech_news.database import db
+from pymongo import ASCENDING, DESCENDING
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    data = (
+        db.news.find({})
+        .sort(
+            [
+                ("shares_count", DESCENDING),
+                ("comments_count", DESCENDING),
+                ("title", ASCENDING),
+            ]
+        ).limit(5)
+    )
+
+    news = []
+
+    for new in data:
+        news.append((new["title"], new["url"]))
+    return news
 
 
 def top_5_categories():

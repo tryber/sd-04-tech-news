b8b838fc09044db8a316fd5575aa307949513292
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-03-14 15:26:33 -0300

    Create search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,13 @@
+from ..database import search_news
+
+
+def serach_by_key(key, query):
+    searched_news = search_news({key: {"$regex": query, "$options": "i"}})
+    return [(new["title"], new["url"]) for new in searched_news]
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    return serach_by_key("title", title)
 
 
 def search_by_date(date):

4cfe78b5ef70edf81a830516ea0d4f0cba561d55
lourenco <loulage@gmail.com>
2021-03-14 01:40:09 -0300

    refactor search_engine due to tuples bug

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,36 +1,45 @@
-from tech_news.database import search_news
-from datetime import datetime
-
-
-def convert_to_tuples(data):
-    if data == []:
-        return []
-    news_container = []
-    for news in news_container:
-        news_container.append((news["title"], news["url"]))
-    return news_container
+from tech_news import database
+import datetime
 
 
 def search_by_title(title):
-    news = search_news({"title": {"$regex": title, "$options": "i"}})
-    return convert_to_tuples(news)
+    news = database.search_news({"title": {"$regex": title, "$options": "-i"}})
+    if len(news) == 0:
+        return news
+
+    return [(news[0]["title"], news[0]["url"])]
 
 
 def search_by_date(date):
     try:
-        newDate = datetime.strptime(date, "%Y-%m-%d")
+        newDate = datetime.datetime.strptime(date, "%Y-%m-%d")
         print(newDate)
-        news = search_news({"timestamp": {"$regex": date, "$options": "i"}})
-        return convert_to_tuples(news)
+        news = database.search_news({"timestamp": {"$regex": date}})
     except ValueError:
         raise ValueError("Data inválida")
+    else:
+        if len(news) == 0:
+            return news
+        return [(news[0]["title"], news[0]["url"])]
 
 
 def search_by_source(source):
-    news = search_news({"sources": {"$regex": source, "$options": "i"}})
-    return convert_to_tuples(news)
+    news = database.search_news(
+        {"sources": {"$regex": source, "$options": "-i"}}
+    )
+
+    if len(news) == 0:
+        return news
+
+    return [(news[0]["title"], news[0]["url"])]
 
 
 def search_by_category(category):
-    news = search_news({"categories": {"$regex": category, "$options": "i"}})
-    return convert_to_tuples(news)
+    news = database.search_news(
+        {"categories": {"$regex": category, "$options": "-i"}}
+    )
+
+    if len(news) == 0:
+        return news
+
+    return [(news[0]["title"], news[0]["url"])]

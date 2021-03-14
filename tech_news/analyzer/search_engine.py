6afb7abd85f43ba3394f3464fc3a7b776447e2fd
fogo5000 <lucaslucas2008@gmail.com>
2021-03-14 05:26:20 -0300

    Requisition 5 - 8 done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,14 +1,41 @@
+from tech_news import database
+from datetime import datetime
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    vazio = []
+    search = database.search_news({"title": {
+        "$regex": title, "$options": "i"}})
+    if len(search) == 0:
+        return vazio
+    return [(search[0]["title"], search[0]["url"])]
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        if datetime.strptime(date, "%Y-%m-%d"):
+            found = []
+            search = database.search_news({"timestamp": {"$regex": date}})
+            for dat in search:
+                found.append((dat["title"], dat["url"]))
+            return found
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    vazio = []
+    search = database.search_news({"sources": {
+        "$regex": source, "$options": "i"}})
+    if len(search) == 0:
+        return vazio
+    return [(search[0]["title"], search[0]["url"])]
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    vazio = []
+    search = database.search_news({"categories": {
+        "$regex": category, "$options": "i"}})
+    if len(search) == 0:
+        return vazio
+    return [(search[0]["title"], search[0]["url"])]

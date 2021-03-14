09749f54d0bf686906483c67c6ffe5ce6a3718f0
gabrielukke <gabriellukk@outlook.com>
2021-03-14 03:57:53 -0300

    Req 4 - all tests

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,36 @@
+import csv
+from tech_news.database import find_news
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    news = find_news()
+
+    if filepath.endswith(".csv"):
+        with open(filepath, "w") as file:
+            headers = [
+                "url",
+                "title",
+                "timestamp",
+                "writer",
+                "shares_count",
+                "comments_count",
+                "summary",
+                "sources",
+                "categories",
+            ]
+
+            writer = csv.writer(file, delimiter=";")
+            writer.writerow(headers)
+
+            new = []
+
+            for key in headers:
+                if type(news[0][key]) == list:
+                    new.append(",".join(news[0][key]))
+                else:
+                    new.append(news[0][key])
+            print("rows", new)
+            writer.writerow(new)
+
+    else:
+        raise ValueError("Formato invalido")
